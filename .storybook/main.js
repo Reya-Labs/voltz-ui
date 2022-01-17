49a4c3d9e3df36467aa20a7d1182252d992e978a
Nicholas Piano <nicholas.d.piano@gmail.com>
2022-01-17 14:04:54 +0100

    fix: storybook compatibility

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,23 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react"
-}
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      loader: '@svgr/webpack',
+    });
+    config.module.rules.push({
+      test: /\.(sa|sc|c)ss$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+    });
+    return config;
+  },
+};
