a7f3006e4d1a4a906211b81b1f683d3cd6092ad0
Nicholas Piano <nicholas.d.piano@gmail.com>
2022-01-26 11:08:07 +0100

    feat: add PixelOperatorMono8-Bold font

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import './fonts/PixelOperator/PixelOperatorMono8.ttf';
+import './fonts/PixelOperator/PixelOperatorMono8-Bold.ttf';
 import './index.css';
 import { ThemeProvider } from './theme';
 import { App } from './application';
