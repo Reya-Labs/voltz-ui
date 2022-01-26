1af63ad9fe9ca974fe8f1039f3d28b92366db0f3
Nicholas Piano <nicholas.d.piano@gmail.com>
2022-01-26 11:39:24 +0100

    chore: add application font test

--- src/application/App/components/Home/Home.tsx
+++ src/application/App/components/Home/Home.tsx
@@ -1,7 +1,21 @@
+import { Background } from '@theme';
 import { Typography } from '@components/atomic';
 
 const Home = () => {
-  return <Typography variant="h1">Home</Typography>;
+  return (
+    <Background>
+      <Typography variant="h1">Heading 1</Typography>
+      <Typography variant="h2">Heading 2</Typography>
+      <Typography variant="h3">Heading 3</Typography>
+      <Typography variant="h4">Heading 4</Typography>
+      <Typography variant="h5">Heading 5</Typography>
+      <Typography variant="h6">Heading 6</Typography>
+      <Typography variant="subtitle1">Subtitle 1</Typography>
+      <Typography variant="subtitle2">Subtitle 2</Typography>
+      <Typography variant="body1">Body 1</Typography>
+      <Typography variant="body2">Body 2</Typography>
+    </Background>
+  );
 };
 
 export default Home;
