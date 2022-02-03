ea0f54840660b1d02a3cf672d061397be32ed29d
Nicholas Piano <nicholas.d.piano@gmail.com>
2022-02-03 12:02:20 +0100

    feat: update Panel

--- src/components/atomic/Panel/Panel.tsx
+++ src/components/atomic/Panel/Panel.tsx
@@ -3,20 +3,49 @@ import { SxProps, Theme } from '@mui/system';
 import Box from '@mui/material/Box';
 
 export type PanelProps = {
-  type: 'error' | 'warning' | 'info';
+  type?: 'error' | 'warning' | 'info' | 'main' | 'dark' | 'darker';
 };
 
 const Panel: React.FunctionComponent<PanelProps> = ({ type, children }) => {
   const commonOverrides: SxProps<Theme> = {
-    border: 2,
+    border: 1,
+    borderColor: 'transparent',
     borderRadius: 2,
     padding: (theme) => theme.spacing(2),
   };
-  const typeStyleOverrides = (): SxProps<Theme> => ({
-    backgroundColor: `${type}.dark`,
-    borderColor: `${type}.main`,
-    color: `${type}.main`,
-  });
+  const typeStyleOverrides = (): SxProps<Theme> => {
+    if (!type) {
+      return {
+        backgroundColor: `primary.dark`,
+      };
+    }
+
+    switch (type) {
+      case 'main':
+        return {
+          backgroundColor: `secondary.darken030`,
+        };
+
+      case 'dark':
+        return {
+          backgroundColor: `secondary.darken040`,
+        };
+
+      case 'darker':
+        return {
+          backgroundColor: `secondary.darken050`,
+        };
+
+      default:
+        return {
+          backgroundColor: `${type}.main`,
+          borderColor: `${type}.light`,
+          '& > *': {
+            color: `${type}.light`,
+          },
+        };
+    }
+  };
 
   return <Box sx={{ ...commonOverrides, ...typeStyleOverrides() }}>{children}</Box>;
 };
