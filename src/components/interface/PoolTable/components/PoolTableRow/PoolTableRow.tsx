a8fe7073e8edcc142dff59a5be4f77009aa6ad1f
Nicholas Piano <nicholas.d.piano@gmail.com>
2022-02-10 12:24:54 +0100

    feat: add cell labels

--- src/components/interface/PoolTable/components/PoolTableRow/PoolTableRow.tsx
+++ src/components/interface/PoolTable/components/PoolTableRow/PoolTableRow.tsx
@@ -4,6 +4,7 @@ import TableCell from '@mui/material/TableCell';
 import { SystemStyleObject, Theme } from '@mui/system';
 
 import { AgentProps, Agents } from '@theme';
+import { Typography } from '@components/atomic';
 import { MaturityInformation } from '@components/composite';
 import { Mode, TableData, TableFields } from '../../types';
 
@@ -26,12 +27,12 @@ const PoolTableRow: React.FunctionComponent<PoolTableRowProps> = ({
       borderColor: 'transparent',
       padding: (theme) => theme.spacing(4),
       '&:first-of-type': {
-        borderTopLeftRadius: 4,
-        borderBottomLeftRadius: 4,
+        borderTopLeftRadius: 8,
+        borderBottomLeftRadius: 8,
       },
       '&:last-of-type': {
-        borderTopRightRadius: 4,
-        borderBottomRightRadius: 4,
+        borderTopRightRadius: 8,
+        borderBottomRightRadius: 8,
       },
     },
   };
@@ -60,45 +61,60 @@ const PoolTableRow: React.FunctionComponent<PoolTableRowProps> = ({
 
   return (
     <TableRow key={key} sx={{ ...commonOverrides, ...typeStyleOverrides() }}>
-      {labels.map(([field, _label]) => {
+      {labels.map(([field, label]) => {
         const renderDisplay = () => {
-          switch (field) {
-            case 'protocol':
-              return datum.protocol;
-
-            case 'fixedApr':
-              return `${datum.fixedApr}%`;
-
-            case 'variableApr': {
-              if (!datum.variableApr) {
-                return 'Variable APR';
-              }
-
-              return `${datum.variableApr}%`;
-            }
-
-            case 'margin': {
-              if (!datum.margin) {
-                return 'Margin';
-              }
-
-              return `$${datum.margin}`;
-            }
-
-            case 'notional': {
-              if (!datum.notional) {
-                return 'Margin';
-              }
-
-              return `$${datum.notional}`;
-            }
-
-            case 'maturity':
-              return <MaturityInformation startDate={datum.startDate} endDate={datum.endDate} />;
-
-            default:
-              return null;
+          if (field === 'maturity') {
+            return (
+              <MaturityInformation
+                label={label}
+                startDate={datum.startDate}
+                endDate={datum.endDate}
+              />
+            );
           }
+
+          const getContent = () => {
+            switch (field) {
+              case 'protocol':
+                return datum.protocol;
+
+              case 'fixedApr':
+                return `${datum.fixedApr}%`;
+
+              case 'variableApr': {
+                if (!datum.variableApr) {
+                  return 'Variable APR';
+                }
+
+                return `${datum.variableApr}%`;
+              }
+
+              case 'margin': {
+                if (!datum.margin) {
+                  return 'Margin';
+                }
+
+                return `$${datum.margin}`;
+              }
+
+              case 'notional': {
+                if (!datum.notional) {
+                  return 'Margin';
+                }
+
+                return `$${datum.notional}`;
+              }
+
+              default:
+                return null;
+            }
+          };
+
+          return (
+            <Typography variant="body2" label={label}>
+              {getContent()}
+            </Typography>
+          );
         };
 
         return <TableCell key={field}>{renderDisplay()}</TableCell>;
