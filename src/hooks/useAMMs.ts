908d7230bb2cbc253be5f0edb2c9a3d09ad0c45a
Nicholas Piano <nicholas.d.piano@gmail.com>
2022-03-18 11:14:48 +0100

    feat: change getVariableApy and add default provider

--- src/hooks/useAMMs.ts
+++ src/hooks/useAMMs.ts
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 import isNull from 'lodash/isNull';
 import { AMM, Token, RateOracle } from '@voltz/v1-sdk';
+import { providers } from 'ethers';
 
 import { useWallet } from '@hooks';
 import { useGetAmMsQuery, Amm_OrderBy } from '@graphql';
@@ -31,6 +32,7 @@ const useAMMs = (): UseAMMsResult => {
         }) =>
           new AMM({
             signer,
+            provider: providers.getDefaultProvider(process.env.REACT_APP_DEFAULT_PROVIDER_NETWORK),
             rateOracle: new RateOracle({ id: rateOracleAddress, protocolId: protocolId as number }),
             underlyingToken: new Token({ id: tokenAddress, name: tokenName }),
             ...rest,
