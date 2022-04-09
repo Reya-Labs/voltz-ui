0de10fb7054c323aceb6da84415e42394412c500
Nicholas Piano <nicholas.d.piano@gmail.com>
2022-04-09 11:18:41 +0200

    feat: modify typography to add agent colors

--- src/components/atomic/Typography/Typography.tsx
+++ src/components/atomic/Typography/Typography.tsx
@@ -1,12 +1,55 @@
 import React from 'react';
+import { SystemStyleObject, Theme } from '@mui/system';
 import MuiTypography, { TypographyProps as MuiTypographyProps } from '@mui/material/Typography';
 
+import { AgentProps, Agents } from '@components/contexts';
+import { useAgentWithOverride } from '@hooks';
 import { withLabel } from '../../utilities';
 
-export type TypographyProps = MuiTypographyProps;
+export type TypographyProps = MuiTypographyProps &
+  AgentProps & {
+    agentStyling?: boolean;
+  };
 
-const Typography: React.FunctionComponent<TypographyProps> = ({ ...props }) => {
-  return <MuiTypography {...props} />;
+const Typography: React.FunctionComponent<TypographyProps> = ({
+  agent: agentOverride,
+  agentStyling = false,
+  sx,
+  ...props
+}) => {
+  const { agent } = useAgentWithOverride(agentOverride);
+  const agentStyleOverrides = (): SystemStyleObject<Theme> => {
+    if (!agent || !agentStyling) {
+      return {};
+    }
+
+    if (agent === Agents.FIXED_TRADER) {
+      return {
+        color: 'primary.base',
+      };
+    }
+
+    if (agent === Agents.VARIABLE_TRADER) {
+      return {
+        color: 'tertiary.base',
+      };
+    }
+
+    if (agent === Agents.LIQUIDITY_PROVIDER) {
+      return {
+        color: 'error.base',
+      };
+    }
+
+    return {};
+  };
+
+  return (
+    <MuiTypography
+      {...props}
+      sx={{ ...(sx as SystemStyleObject<Theme>), ...agentStyleOverrides() }}
+    />
+  );
 };
 
 export default withLabel<TypographyProps>(Typography);
