8ca9b445c39a4b1476a0cd3722871a3938b2fd6f
Alex Davies <alexdavies@voltz.xyz>
2022-04-22 22:33:52 +0100

    refactor: made empty positions message match designs

--- src/application/App/components/ConnectedPositionTable/ConnectedPositionTable.tsx
+++ src/application/App/components/ConnectedPositionTable/ConnectedPositionTable.tsx
@@ -5,10 +5,10 @@ import { useNavigate } from 'react-router-dom';
 import { data } from '@utilities';
 import { usePositions } from '@hooks';
 import { PositionTable, PositionTableFields } from '@components/interface';
-import { Button, Panel, Typography } from '@components/atomic';
+import { Panel } from '@components/atomic';
 import { Agents } from '@components/contexts';
-import { Box } from '@mui/material';
 import { routes } from '@routes';
+import { RouteLink } from '@components/atomic';
 
 export type ConnectedAMMTableProps = {
   onSelectItem: (item: Position) => void;
@@ -27,35 +27,22 @@ const ConnectedPositionTable: React.FunctionComponent<ConnectedAMMTableProps> = 
   const pages = 0;
   const navigate = useNavigate();
 
-  const handleViewPools = useCallback(() => {
-    if(agent === Agents.LIQUIDITY_PROVIDER) {
-      navigate(`/${routes.POOLS}`);
-    } else {
-      navigate(`/${routes.SWAP}`);
-    }
-  }, [useNavigate])
-
   if(loading || error) {
     return null;
   }
 
   if (!positionsByAgent) {
     return (
-      <Panel variant='dark' sx={{textAlign: 'center'}}>
-        <Typography >You do not currently have any positions</Typography>
-        <Box sx={{ marginTop: (theme) => theme.spacing(4) }}>
-          <Button variant="contained" onClick={handleViewPools} sx={{
-            paddingTop: (theme) => theme.spacing(3),
-            paddingBottom: (theme) => theme.spacing(3),
-            paddingLeft: (theme) => theme.spacing(4),
-            paddingRight: (theme) => theme.spacing(4),
-            fontSize: 18,
-            lineHeight: 1,
-            boxShadow: 'none',
-          }}>
-            VIEW POOLS
-          </Button>
-        </Box>
+      <Panel variant='dark' sx={{textAlign: 'center', marginTop: 10 }}>
+        <Panel variant='main' sx={{
+          width: '100%', 
+          maxWidth: '900px', 
+          textAlign: 'center',
+        }}>
+          <RouteLink to={agent === Agents.LIQUIDITY_PROVIDER ? `/${routes.POOLS}` : `/${routes.SWAP}`}>
+            OPEN YOUR FIRST POSITION
+          </RouteLink>
+        </Panel>
       </Panel>
     )
   }
---
+++ src/components/atomic/RouteLink/RouteLink.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link, LinkProps } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import { useAgent } from '@hooks';
+import { Agents } from 'src/components/contexts/AgentContext/types';
+
+const StyledRouteLink = styled(Link)<LinkProps>(({  }) => ({}));
+
+const RouteLink = ({ children, to, ...props }: LinkProps) => {
+  const agentCtx = useAgent();
+
+  return (
+    <StyledRouteLink to={to} {...props} sx={{  
+      fontSize: '18px',
+      lineHeight: '14px',
+      letterSpacing: '0.36px',
+      fontFamily: 'PixelOperatorMono',
+      color: () => {
+        if (agentCtx.agent === Agents.FIXED_TRADER) return 'primary.base';
+        if (agentCtx.agent === Agents.VARIABLE_TRADER) return 'tertiary.base';
+        return 'error.base';
+      },
+    }}>
+      {children}
+    </StyledRouteLink>
+  );
+}
+
+export default RouteLink;
