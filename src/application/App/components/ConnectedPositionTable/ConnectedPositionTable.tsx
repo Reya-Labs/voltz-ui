b02c50da769d8bc82aec1408cf17ae7ba827d2c9
Alex Davies <alexdavies@voltz.xyz>
2022-04-22 15:25:17 +0100

    feat: added message to portfolio routes when you do not have any positions yet

--- src/application/App/components/ConnectedPositionTable/ConnectedPositionTable.tsx
+++ src/application/App/components/ConnectedPositionTable/ConnectedPositionTable.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Position } from '@voltz/v1-sdk';
+import { useNavigate } from 'react-router-dom';
 
 import { data } from '@utilities';
 import { usePositions } from '@hooks';
 import { PositionTable, PositionTableFields } from '@components/interface';
+import { Button, Panel, Typography } from '@components/atomic';
 import { Agents } from '@components/contexts';
+import { Box } from '@mui/material';
+import { routes } from '@routes';
 
 export type ConnectedAMMTableProps = {
   onSelectItem: (item: Position) => void;
@@ -21,9 +25,39 @@ const ConnectedPositionTable: React.FunctionComponent<ConnectedAMMTableProps> = 
   const [size, setSize] = useState<number | null>(null);
   const { positionsByAgent, loading, error } = usePositions();
   const pages = 0;
+  const navigate = useNavigate();
 
-  if (!positionsByAgent || loading || error) {
+  const handleViewPools = useCallback(() => {
+    if(agent === Agents.LIQUIDITY_PROVIDER) {
+      navigate(`/${routes.POOLS}`);
+    } else {
+      navigate(`/${routes.SWAP}`);
+    }
+  }, [useNavigate])
+
+  if(loading || error) {
     return null;
+  }
+
+  if (!positionsByAgent) {
+    return (
+      <Panel variant='dark' sx={{textAlign: 'center'}}>
+        <Typography >You do not currently have any positions</Typography>
+        <Box sx={{ marginTop: (theme) => theme.spacing(4) }}>
+          <Button variant="contained" onClick={handleViewPools} sx={{
+            paddingTop: (theme) => theme.spacing(3),
+            paddingBottom: (theme) => theme.spacing(3),
+            paddingLeft: (theme) => theme.spacing(4),
+            paddingRight: (theme) => theme.spacing(4),
+            fontSize: 18,
+            lineHeight: 1,
+            boxShadow: 'none',
+          }}>
+            VIEW POOLS
+          </Button>
+        </Box>
+      </Panel>
+    )
   }
 
   return (
@@ -45,3 +79,4 @@ const ConnectedPositionTable: React.FunctionComponent<ConnectedAMMTableProps> = 
 };
 
 export default ConnectedPositionTable;
+
