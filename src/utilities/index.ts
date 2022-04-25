5a4096db5248840c9f8a4a2b11be99145120b451
Alex Davies <alexdavies@voltz.xyz>
2022-04-25 22:23:24 +0100

    feat: added OverrideTypes utility to simplify overriding component prop types

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -3,3 +3,4 @@ export { default as AugmentedAMM } from './augmentedAmm';
 export { default as calculateNotionalAmount } from './calculateNotionalAmount';
 export { default as elideAddress } from './elideAddress';
 export { default as getErrorMessage } from './getErrorMessage';
+export * from './overrideTypes';
---
+++ src/utilities/overrideTypes.ts
@@ -0,0 +1,5 @@
+// Use this when overriding types 
+// https://dev.to/vborodulin/ts-how-to-override-properties-with-type-intersection-554l
+export type OverrideTypes<T1, T2> = Omit<T1, keyof T2> & T2;
+
+export default OverrideTypes;
