452cf5ccbb6f75efb9a92a8daca2dec0fc03298f
Alex Davies <alexdavies@voltz.xyz>
2022-04-28 12:12:18 +0100

    style: added neon squares to app background

--- src/components/atomic/index.ts
+++ src/components/atomic/index.ts
@@ -13,3 +13,5 @@ export { default as Slider } from './Slider/Slider';
 export { default as ToggleButton } from './ToggleButton/ToggleButton';
 export { default as Typography } from './Typography/Typography';
 export { default as RouteLink } from './RouteLink/RouteLink';
+export { default as Neons } from './Neons/Neons';
+export type { NeonsProps } from './Neons/Neons';
