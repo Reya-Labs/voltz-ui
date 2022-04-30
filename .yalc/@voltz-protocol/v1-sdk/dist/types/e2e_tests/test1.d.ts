a7abc379d1dd1d8e374d41148be18eb5696ac9c3
tudor47 <tudorcostin7@gmail.com>
2022-04-30 16:25:30 +0300

    fix: merge

--- .yalc/@voltz-protocol/v1-sdk/dist/types/e2e_tests/test1.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=test1.d.ts.map
--- .yalc/@voltz-protocol/v1-sdk/src/utils/mapProtocolIdToProtocol.ts
+++
@@ -1,9 +0,0 @@
-const mapProtocolIdToProtocol = (protocolId: number): string => {
-  if (protocolId === 1) {
-    return 'AAVE V2';
-  }
-
-  return 'AAVE V2';
-};
-
-export default mapProtocolIdToProtocol;
