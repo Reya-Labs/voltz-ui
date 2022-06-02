7db64118a9f81cac00e8581c3fb06a7a2047183b
Soleiman Anwary <soleiman.anwary@gmail.com>
2022-06-02 18:01:57 +0100

    feat: show error messages for errors that don't trigger the swap info

--- src/hooks/useAsyncFunction.ts
+++ src/hooks/useAsyncFunction.ts
@@ -3,7 +3,8 @@ import useDebounceFunc from './useDebounceFunc';
 
 export type UseAsyncFunctionResult<ArgsType, ResultType> = {
   result: ResultType | null;
-  error: boolean;
+  errorMessage: string | null;
+  // error: boolean;
   loading: boolean;
   call: (args?: ArgsType) => void;
 };
@@ -15,18 +16,19 @@ const useAsyncFunction = <ArgsType, ResultType>(
 ): UseAsyncFunctionResult<ArgsType, ResultType> => {
   const [args, setArgs] = useState<ArgsType>();
   const [result, setResult] = useState<ResultType | null>(null);
-  const [error, setError] = useState(false);
+  // const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
   const [called, setCalled] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
   const call = useCallback(
     (callArgs?: ArgsType) => {
       setArgs(callArgs);
       setResult(null);
-      setError(false);
+      // setError(false);
       setLoading(false);
       setCalled(true);
     },
-    [setArgs, setResult, setError, setLoading, setCalled],
+    [setArgs, setResult, setLoading, setCalled],
   );
   const request = useRef<Promise<ResultType>>();
 
@@ -49,8 +51,15 @@ const useAsyncFunction = <ArgsType, ResultType>(
         setResult(data);
       }
     } catch (_error) {
+      // eslint-disable-next-line
+      console.log('_error: ', _error)
+      if (_error instanceof Error) {
+        setErrorMessage(_error.message)
+      } else {
+        setErrorMessage('Unrecognized Error')
+      }
       setResult(null);
-      setError(true);
+      // setError(true);
     }
 
     setLoading(false);
@@ -67,11 +76,12 @@ const useAsyncFunction = <ArgsType, ResultType>(
   return useMemo(
     () => ({
       result,
-      error,
+      errorMessage,
+      // error,
       loading,
       call,
     }),
-    [result, error, loading, call],
+    [result, errorMessage, loading, call],
   );
 };
 
