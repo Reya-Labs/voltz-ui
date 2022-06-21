ddd919573673a9c0cc4ab03ae29f8815963be6eb
Alex Davies <alexdavies@voltz.xyz>
2022-06-21 13:42:19 +0100

    refactor: the suffix on the MaskedIntegerField is no longer part of the field itself

--- src/theme/themes/dark/sharedStyles.ts
+++ src/theme/themes/dark/sharedStyles.ts
@@ -5,27 +5,82 @@
 import { SystemStyleObject, Theme } from '@mui/system';
 import colors from '../../colors';
 
-export const inputStyles = (disabled?: boolean, error?: boolean, inputSize?: string, dynamic?: boolean): SystemStyleObject<Theme> => {
-  const styles:SystemStyleObject<Theme> = {
-    fontFamily: 'PixelOperatorMono',
-    backgroundColor: (theme) => theme.palette.secondary.darken040,
-    borderColor: (theme) => {
-      if (error) return theme.palette.error.darken010;
-      return theme.palette.secondary.darken040;
+export const inputStyles = (
+  disabled?: boolean,
+  error?: boolean,
+  inputSize?: string,
+  dynamic?: boolean,
+  suffixPadding: number = 0,
+): SystemStyleObject<Theme> => {
+  const containerStyles:SystemStyleObject<Theme> = {
+    '&:hover': {
+      '*, input': {
+        borderColor: (theme) => {
+          if (error) return theme.palette.error.darken010;
+          if (disabled) return 'transparent';
+          return colors.vzGreyDarkish2;
+        },
+        color: (theme) => {
+          if (error) return theme.palette.error.base;
+          if (disabled) return colors.vzGreyDarkish2;
+          if (dynamic) return colors.lavenderWeb.darken010;
+          return colors.lavenderWeb.darken015
+        },
+        '::placeholder': {
+          color: (theme) => {
+            if (error) return theme.palette.error.base;
+            if (disabled) return colors.vzGreyDarkish2;
+            if (dynamic) return colors.lavenderWeb.darken010;
+            return colors.lavenderWeb.darken015
+          },
+        }
+      }
     },
-    borderStyle: 'solid',
-    borderWidth: '1px',
-    borderRadius: (theme) => theme.spacing(1),
+    '&:focus-within': {
+      '*, input': {
+        outline: 'none',
+        borderColor: (theme) => {
+          if (error) return theme.palette.error.darken010;
+          return colors.lavenderWeb.darken015;
+        },
+        color: (theme) => {
+          if (error) return theme.palette.error.base;
+          return colors.lavenderWeb.base
+        },
+        '::placeholder': {
+          color: (theme) => {
+            if (error) return theme.palette.error.base;
+            return colors.lavenderWeb.base
+          }
+        }
+      }
+    },
+  }
+
+  const childElementStyles:SystemStyleObject<Theme> = {
     color: (theme) => {
       if (error) return theme.palette.error.base;
       if (dynamic) return colors.lavenderWeb.darken010;
       return colors.vzGreyDarkish2;
     },
+    fontFamily: 'PixelOperatorMono',
+    transition: 'border-color 0.1s linear, color 0.1s linear',
+  }
+
+  const inputFieldStyles:SystemStyleObject<Theme> = {
+    backgroundColor: (theme) => theme.palette.secondary.darken040,
+    borderStyle: 'solid',
+    borderWidth: '1px',
+    borderRadius: (theme) => theme.spacing(1),
+    borderColor: (theme) => {
+      if (error) return theme.palette.error.darken010;
+      return theme.palette.secondary.darken040;
+    },
     minHeight: 'auto !important',
     boxSizing: 'border-box',
     width: '100%',
     cursor: disabled ? 'not-allowed' : undefined,
-    transition: 'border-color 0.1s linear, color 0.1s linear',
+    
     '::placeholder': {
       transition: 'color 0.1s linear',
       color: (theme) => {
@@ -34,44 +89,6 @@ export const inputStyles = (disabled?: boolean, error?: boolean, inputSize?: str
         if (dynamic) return colors.lavenderWeb.darken010;
         return colors.vzGreyDarkish2;
       },
-    },
-    '&:hover': {
-      borderColor: (theme) => {
-        if (error) return theme.palette.error.darken010;
-        if (disabled) return 'transparent';
-        return colors.vzGreyDarkish2;
-      },
-      color: (theme) => {
-        if (error) return theme.palette.error.base;
-        if (disabled) return colors.vzGreyDarkish2;
-        if (dynamic) return colors.lavenderWeb.darken010;
-        return colors.lavenderWeb.darken015
-      },
-      '::placeholder': {
-        color: (theme) => {
-          if (error) return theme.palette.error.base;
-          if (disabled) return colors.vzGreyDarkish2;
-          if (dynamic) return colors.lavenderWeb.darken010;
-          return colors.lavenderWeb.darken015
-        },
-      }
-    },
-    '&:focus-visible': {
-      outline: 'none',
-      borderColor: (theme) => {
-        if (error) return theme.palette.error.darken010;
-        return colors.lavenderWeb.darken015;
-      },
-      color: (theme) => {
-        if (error) return theme.palette.error.base;
-        return colors.lavenderWeb.base
-      },
-      '::placeholder': {
-        color: (theme) => {
-          if (error) return theme.palette.error.base;
-          return colors.lavenderWeb.base
-        }
-      }
     },
     "::-webkit-outer-spin-button": { 
       "webkitAppearance": "none", 
@@ -83,33 +100,66 @@ export const inputStyles = (disabled?: boolean, error?: boolean, inputSize?: str
       "mozAppearance": "none",
       "appearance": "none"
     },
-  }
+  };
 
   if(inputSize === 'small') {
     return {
-      ...styles,
-      fontSize: '14px',
-      lineHeight: '14px',
-      height: '31px',
-      padding: (theme) => `calc(${theme.spacing(2)} - 1px) calc(${theme.spacing(4)} - 1px)`,
+      ...containerStyles,
+      '*, input': {
+        ...childElementStyles,
+        fontSize: '14px',
+        lineHeight: '14px',
+      },
+      'input': {
+        ...inputFieldStyles,
+        height: '31px',
+        padding: (theme) => `
+          calc(${theme.spacing(2)} - 1px) 
+          calc(${theme.spacing(4)} - 1px + ${suffixPadding}px) 
+          calc(${theme.spacing(2)} - 1px) 
+          calc(${theme.spacing(4)} - 1px)
+        `,
+      }
     }
   }
 
   if(inputSize === 'medium') {
     return {
-      ...styles,
-      fontSize: '16px',
-      lineHeight: '14px',
-      height: '31px',
-      padding: (theme) => `calc(${theme.spacing(2)} - 1px) calc(${theme.spacing(4)} - 1px)`,
+      ...containerStyles,
+      '*, input': {
+        ...childElementStyles,
+        fontSize: '16px',
+        lineHeight: '14px',
+      },
+      'input': {
+        ...inputFieldStyles,
+        height: '31px',
+        padding: (theme) => `
+          calc(${theme.spacing(2)} - 1px) 
+          calc(${theme.spacing(4)} - 1px + ${suffixPadding}px) 
+          calc(${theme.spacing(2)} - 1px) 
+          calc(${theme.spacing(4)} - 1px)
+        `,
+      }
     }
   }
 
   return {
-    ...styles,
-    fontSize: '24px',
-    lineHeight: '1.2',
-    height: '61px',
-    padding: (theme) => `calc(${theme.spacing(4)} - 1px)`
+    ...containerStyles,
+    '*, input': {
+      ...childElementStyles,
+      fontSize: '24px',
+      lineHeight: '1.2',
+    },
+    'input': {
+      ...inputFieldStyles,
+      height: '61px',
+      padding: (theme) => `
+        calc(${theme.spacing(4)} - 1px)
+        calc(${theme.spacing(4)} - 1px + ${suffixPadding}px)
+        calc(${theme.spacing(4)} - 1px)
+        calc(${theme.spacing(4)} - 1px)
+      `
+    }
   }
 };
