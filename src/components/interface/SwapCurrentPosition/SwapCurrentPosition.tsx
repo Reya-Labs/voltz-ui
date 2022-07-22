7b80c089b73f2a33e504f1eade950d9e1d769fcb
Alex Davies <alexdavies@voltz.xyz>
2022-07-22 16:26:12 +0100

    feat: added cashflow / rollover balance to swap current position info

--- src/components/interface/SwapCurrentPosition/SwapCurrentPosition.tsx
+++ src/components/interface/SwapCurrentPosition/SwapCurrentPosition.tsx
@@ -25,7 +25,7 @@ const SwapCurrentPosition: React.FunctionComponent<SwapCurrentPositionProps> = (
   // }
   const { positionInfo } = usePositionContext();
 
-  const currentPositionBadgeText = `${positionInfo?.result?.beforeMaturity ? 'Current' : 'Previous'} position: ${position.positionType === 1 ? 'Fix taker' : 'Variable taker'}`;
+  const currentPositionBadgeText = `${positionInfo?.result?.beforeMaturity === false ? 'Previous' : 'Current'} position: ${position.positionType === 1 ? 'Fix taker' : 'Variable taker'}`;
   const notional = Math.abs(position.effectiveVariableTokenBalance);
   const margin = position.amm.descale(BigNumber.from(position.margin.toString()));
   const leverage = notional / margin;
@@ -65,6 +65,45 @@ const SwapCurrentPosition: React.FunctionComponent<SwapCurrentPositionProps> = (
     }
   }
 
+  const rows: React.ComponentProps<typeof SummaryPanel>['rows'] = [
+    {
+      label: 'NOTIONAL',
+      value: `${formatCurrency(notional)} ${underlyingTokenName}`
+    },
+    {
+      label: 'LEVERAGE',
+      value: `${formatCurrency(leverage)}x`
+    },
+    {
+      label: 'HEALTH FACTOR',
+      value: positionInfo?.loading ? <Ellipsis/> : getHealthFactor(),
+    },
+    {
+      label: 'CURRENT MARGIN',
+      value: `${formatCurrency(margin)} ${underlyingTokenName}`,
+      highlight: true
+    },
+  ];
+
+  if(formMode === SwapFormModes.ROLLOVER) {
+    rows.push({
+      label: 'CASHFLOW',
+      value: positionInfo?.result
+        ? `${formatCurrency(positionInfo?.result?.accruedCashflow)} ${underlyingTokenName}`
+        : <Ellipsis />,
+      highlight: true
+    });
+
+    rows.push({
+      label: 'ROLLOVER BALANCE',
+      value: positionInfo?.result
+        ? `${formatCurrency(positionInfo?.result?.accruedCashflow + margin)} ${underlyingTokenName}`
+        : <Ellipsis />,
+      highlight: true,
+      bold: true
+    });
+  };
+
   useEffect(() => {
     positionInfo?.call(position);
   }, [position]);
@@ -75,25 +114,10 @@ const SwapCurrentPosition: React.FunctionComponent<SwapCurrentPositionProps> = (
         <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: (theme) => theme.spacing(6) }}>
           <PositionBadge size='small' variant='FC' text={currentPositionBadgeText} sx={{ display: 'inline-block', marginLeft: 0 }} />
         </Box>
-        <SummaryPanel label="Position information" rows={[
-          {
-            label: 'NOTIONAL',
-            value: `${formatCurrency(notional)} ${underlyingTokenName}`
-          },
-          {
-            label: 'LEVERAGE',
-            value: `${formatCurrency(leverage)}x`
-          },
-          {
-            label: 'HEALTH FACTOR',
-            value: positionInfo?.loading ? <Ellipsis/> : getHealthFactor(),
-          },
-          {
-            label: 'CURRENT MARGIN',
-            value: `${formatCurrency(margin)} ${underlyingTokenName}`,
-            highlight: true
-          },
-        ]} />
+        <SummaryPanel 
+          label="Position information" 
+          rows={rows} 
+        />
         <Button
           sx={{ 
             marginTop: (theme) => theme.spacing(6), 
