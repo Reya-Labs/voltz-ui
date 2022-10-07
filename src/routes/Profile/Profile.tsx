0c957350431769af5d6b3ad662e9960a3988a75d
ssbarbee <ssbarbee@users.noreply.github.com>
2022-10-07 14:09:55 +0200

    feat: add title/paragraph for profile

--- src/routes/Profile/Profile.tsx
+++ src/routes/Profile/Profile.tsx
@@ -1,20 +1,41 @@
 import React from 'react';
-
+import { Panel, Typography } from '@components/atomic';
+import { SystemStyleObject, Theme } from '@theme';
 import Box from '@mui/material/Box';
 import { Page } from '@components/interface';
+
+const labelStyles: SystemStyleObject<Theme> = {
+  textTransform: 'uppercase',
+  fontWeight: 400,
+  fontSize: 14,
+  color: '#B3AFC6',
+};
+
+const titleStyles: SystemStyleObject<Theme> = {
+  fontSize: '40px',
+  lineHeight: '1.2',
+  marginTop: (theme) => theme.spacing(2),
+};
 
 const Profile: React.FunctionComponent = () => {
   return (
     <Page>
-      <Box
-        sx={{
-          height: '100%',
-          display: 'flex',
-          justifyContent: 'center',
-          backdropFilter: 'blur(8px)',
-        }}
-      >
-        Profile
+      <Box sx={{ backdropFilter: 'blur(8px)', height: '100%', paddingBottom: '200px' }}>
+        <Panel
+          variant="dark"
+          padding="small"
+          sx={{ width: '100%', maxWidth: '800px', margin: '0 auto', background: 'transparent' }}
+        >
+          <Box sx={{ textTransform: 'uppercase' }}>
+            <Typography variant="h1" sx={titleStyles}>
+              VOLTZ COMMUNITY ON CHAIN
+            </Typography>
+            <Typography variant="subtitle1" sx={labelStyles}>
+              Compete against other traders for rewards by trading or bringing people to the
+              platform.
+            </Typography>
+          </Box>
+        </Panel>
       </Box>
     </Page>
   );
