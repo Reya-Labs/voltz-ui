53ee194be0700d1e1f19b383d4d4dba19c7c2c4a
ssbarbee <ssbarbee@users.noreply.github.com>
2022-10-16 18:09:20 +0200

    feat: add loading indicator for badges

--- src/routes/Profile/BadgeCard/BadgeCard.tsx
+++ src/routes/Profile/BadgeCard/BadgeCard.tsx
@@ -6,11 +6,14 @@ import { Badge } from '../Badge/Badge';
 import { BadgeVariant } from '../types';
 import { BadgePill } from '../BadgePill/BadgePill';
 import { BADGE_VARIANT_DESCRIPTION_COPY_MAP, BADGE_VARIANT_TITLE_COPY_MAP } from '../helpers';
+import Skeleton from '@mui/material/Skeleton';
 
 export type BadgeCardProps = {
   variant: BadgeVariant;
+  loading?: boolean;
 };
-export const BadgeCard: React.FunctionComponent<BadgeCardProps> = ({ variant }) => {
+
+export const BadgeCard: React.FunctionComponent<BadgeCardProps> = ({ loading, variant }) => {
   return (
     <Box
       data-testid="BadgeCard"
@@ -27,7 +30,7 @@ export const BadgeCard: React.FunctionComponent<BadgeCardProps> = ({ variant }) 
           marginBottom: (theme) => theme.spacing(6),
         }}
       >
-        <BadgePill variant={variant} />
+        <BadgePill loading={loading} variant={variant} />
       </Box>
       <Box
         sx={{
@@ -36,31 +39,39 @@ export const BadgeCard: React.FunctionComponent<BadgeCardProps> = ({ variant }) 
           marginBottom: (theme) => theme.spacing(6),
         }}
       >
-        <Badge variant={variant} />
+        <Badge loading={loading} variant={variant} />
       </Box>
-      <Typography
-        variant="body2"
-        sx={{
-          color: colors.lavenderWeb.base,
-          fontSize: '18px',
-          lineHeight: '24px',
-          fontWeight: 700,
-          marginBottom: (theme) => theme.spacing(2),
-        }}
-      >
-        {BADGE_VARIANT_TITLE_COPY_MAP[variant].toUpperCase()}
-      </Typography>
-      <Typography
-        variant="body2"
-        sx={{
-          color: colors.lavenderWeb.base,
-          fontSize: '12px',
-          lineHeight: '18px',
-          fontWeight: 400,
-        }}
-      >
-        {BADGE_VARIANT_DESCRIPTION_COPY_MAP[variant]}
-      </Typography>
+      {loading ? (
+        <Skeleton variant="text" sx={{ fontSize: '18px', lineHeight: '24px' }} />
+      ) : (
+        <Typography
+          variant="body2"
+          sx={{
+            color: colors.lavenderWeb.base,
+            fontSize: '18px',
+            lineHeight: '24px',
+            fontWeight: 700,
+            marginBottom: (theme) => theme.spacing(2),
+          }}
+        >
+          {BADGE_VARIANT_TITLE_COPY_MAP[variant].toUpperCase()}
+        </Typography>
+      )}
+      {loading ? (
+        <Skeleton variant="text" sx={{ fontSize: '12px', lineHeight: '18px' }} />
+      ) : (
+        <Typography
+          variant="body2"
+          sx={{
+            color: colors.lavenderWeb.base,
+            fontSize: '12px',
+            lineHeight: '18px',
+            fontWeight: 400,
+          }}
+        >
+          {BADGE_VARIANT_DESCRIPTION_COPY_MAP[variant]}
+        </Typography>
+      )}
     </Box>
   );
 };
