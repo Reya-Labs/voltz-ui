f928363c839206db3d5d04c88c6d61dccd478776
ssbarbee <ssbarbee@users.noreply.github.com>
2022-10-16 18:09:20 +0200

    feat: add loading indicator for badges

--- src/routes/Profile/CollectionBadge/CollectionBadge.tsx
+++ src/routes/Profile/CollectionBadge/CollectionBadge.tsx
@@ -5,60 +5,75 @@ import { Typography } from '@components/atomic';
 import { BadgeVariant } from '../types';
 import { BadgePill } from '../BadgePill/BadgePill';
 import { BADGE_VARIANT_TITLE_COPY_MAP } from '../helpers';
+import Skeleton from '@mui/material/Skeleton';
 
 export type CollectionBadgeProps = {
   achievedAt?: string;
   variant: BadgeVariant;
+  loading?: boolean;
 };
 export const CollectionBadge: React.FunctionComponent<CollectionBadgeProps> = ({
   achievedAt,
   variant,
-}) => (
-  <Box
-    data-testid={`CollectionBadge-${variant}`}
-    sx={{
-      backgroundColor: '#2B2548',
-      borderRadius: '8px',
-      padding: (theme) => theme.spacing(2, 2, 2, 4),
-      display: 'flex',
-      flexDirection: 'row',
-      opacity: achievedAt ? 1 : 0.7,
-      alignItems: 'center',
-    }}
-  >
+  loading,
+}) =>
+  loading ? (
+    <Skeleton
+      variant="rectangular"
+      sx={{
+        padding: (theme) => theme.spacing(2, 2, 2, 4),
+        borderRadius: '8px',
+        fontSize: '18px',
+        lineHeight: '24px',
+        width: '100%',
+      }}
+    />
+  ) : (
     <Box
+      data-testid={`CollectionBadge-${variant}`}
       sx={{
-        width: '100px',
+        backgroundColor: '#2B2548',
+        borderRadius: '8px',
+        padding: (theme) => theme.spacing(2, 2, 2, 4),
+        display: 'flex',
+        flexDirection: 'row',
+        opacity: achievedAt ? 1 : 0.7,
+        alignItems: 'center',
       }}
     >
-      <Box>
-        <BadgePill variant={variant} />
+      <Box
+        sx={{
+          width: '100px',
+        }}
+      >
+        <Box>
+          <BadgePill variant={variant} />
+        </Box>
       </Box>
+      <Typography
+        variant="body2"
+        sx={{
+          color: colors.lavenderWeb.base,
+          fontSize: '18px',
+          lineHeight: '24px',
+          fontWeight: 400,
+          flex: '1',
+        }}
+      >
+        {BADGE_VARIANT_TITLE_COPY_MAP[variant].toUpperCase()}
+      </Typography>
+      <Typography
+        variant="body2"
+        sx={{
+          color: colors.lavenderWeb.base,
+          fontSize: '18px',
+          lineHeight: '24px',
+          fontWeight: 400,
+          padding: (theme) => theme.spacing(0, 4),
+          textAlign: 'right',
+        }}
+      >
+        {achievedAt ? `Achieved: ${achievedAt}` : 'Keep trading...'}
+      </Typography>
     </Box>
-    <Typography
-      variant="body2"
-      sx={{
-        color: colors.lavenderWeb.base,
-        fontSize: '18px',
-        lineHeight: '24px',
-        fontWeight: 400,
-        flex: '1',
-      }}
-    >
-      {BADGE_VARIANT_TITLE_COPY_MAP[variant].toUpperCase()}
-    </Typography>
-    <Typography
-      variant="body2"
-      sx={{
-        color: colors.lavenderWeb.base,
-        fontSize: '18px',
-        lineHeight: '24px',
-        fontWeight: 400,
-        padding: (theme) => theme.spacing(0, 4),
-        textAlign: 'right',
-      }}
-    >
-      {achievedAt ? `Achieved: ${achievedAt}` : 'Keep trading...'}
-    </Typography>
-  </Box>
-);
+  );
