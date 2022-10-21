09875bb8d6bc730435d36b4a927190a86eed1c4e
ssbarbee <ssbarbee@users.noreply.github.com>
2022-10-21 10:18:05 +0200

    feat: make leaderboard more lively

---
+++ src/components/interface/RankingTable/components/Points.tsx
@@ -0,0 +1,41 @@
+import { Typography } from '@components/atomic';
+import { colors } from '@theme';
+import CountUp from 'react-countup';
+import { formatNumber } from '@utilities';
+import React from 'react';
+
+type PointsProps = {
+  points: number;
+};
+
+const PointsTypography: React.FunctionComponent = ({ children }) => (
+  <Typography
+    variant="body2"
+    sx={{
+      color: colors.lavenderWeb.base,
+      fontSize: '18px',
+      lineHeight: '24px',
+      fontWeight: 400,
+      padding: (theme) => theme.spacing(0, 4),
+      textAlign: 'right',
+    }}
+  >
+    {children}
+  </Typography>
+);
+
+export const Points: React.FunctionComponent<PointsProps> = ({ points }) => {
+  if (points <= 0) {
+    return <PointsTypography>---</PointsTypography>;
+  }
+
+  return (
+    <CountUp start={0} end={points} delay={0} formattingFn={formatNumber}>
+      {({ countUpRef }) => (
+        <PointsTypography>
+          <span ref={countUpRef} />
+        </PointsTypography>
+      )}
+    </CountUp>
+  );
+};
