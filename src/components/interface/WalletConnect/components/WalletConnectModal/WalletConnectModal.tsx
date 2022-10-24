21b3666f625e77673a1e0724e44151c3477ab7d7
Sol <35786191+Sol3iman@users.noreply.github.com>
2022-10-24 16:47:16 +0100

    fix: when disconnect button is pressed, the right flow is invoked (#280)
    * fix: when disconnect button is pressed, the right flow is invoked
    
    * fix: make if clause readable

--- src/components/interface/WalletConnect/components/WalletConnectModal/WalletConnectModal.tsx
+++ src/components/interface/WalletConnect/components/WalletConnectModal/WalletConnectModal.tsx
@@ -5,6 +5,7 @@ import { Panel } from '@components/atomic';
 import { Modal } from '@components/composite';
 import { WalletConnectButton, WalletDisplay, WalletSelect } from './components';
 import { WalletName } from '@contexts';
+import { walletOptions } from './components/WalletSelect/WalletSelect';
 
 const WalletConnectModal: React.FunctionComponent = () => {
   const wallet = useWallet();
@@ -19,7 +20,11 @@ const WalletConnectModal: React.FunctionComponent = () => {
   };
   const handleSelectWallet = (name: WalletName) => {
     handleClose();
-    wallet.connect(name);
+    if (name === 'disconnect') {
+      wallet.disconnect();
+    } else {
+      wallet.connect(name);
+    }
   };
 
   const renderContent = () => {
