b0f48f61536b7e11c5d24b7c51800e3e1e0fb067
ssbarbee <ssbarbee@users.noreply.github.com>
2022-10-31 15:03:28 +0100

    fix: added loader indicator for the position and vault info

--- src/components/interface/MellowLPTable/components/MellowLPPosition.tsx
+++ src/components/interface/MellowLPTable/components/MellowLPPosition.tsx
@@ -1,6 +1,7 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Skeleton, Typography } from '@mui/material';
 import { AugmentedMellowLpVault, formatCurrency } from '@utilities';
 import { isUndefined } from 'lodash';
+import React from 'react';
 
 export type MellowLPPositionProps = {
   lpVault: AugmentedMellowLpVault;
@@ -16,35 +17,57 @@ const MellowLPPosition: React.FunctionComponent<MellowLPPositionProps> = ({
   const getPositionInfo = () => {
     if (isUndefined(lpVault.userDeposit)) {
       return (
-        <Typography
-          variant="h6"
-          sx={{ fontSize: '14px', color: '#9B97AD', textTransform: 'uppercase' }}
-        >
-          Your position ---
-        </Typography>
+        <Skeleton
+          variant="text"
+          sx={{ width: '100%', fontSize: '14px', lineHeight: '20px', padding: '4px' }}
+        />
       );
     }
 
     return (
-      <Box sx={{ display: 'flex' }}>
-        <Typography
-          variant="h6"
-          sx={{ fontSize: '14px', color: '#9B97AD', textTransform: 'uppercase' }}
-        >
-          Your position:
-        </Typography>
-        <Typography
-          variant="h6"
+      <>
+        <Box sx={{ display: 'flex' }}>
+          <Typography
+            variant="h6"
+            sx={{ fontSize: '14px', color: '#9B97AD', textTransform: 'uppercase' }}
+          >
+            Your position:
+          </Typography>
+          <Typography
+            variant="h6"
+            sx={{
+              fontSize: '14px',
+              color: 'primary.light',
+              textTransform: 'uppercase',
+              marginLeft: '4px',
+            }}
+          >
+            {formatCurrency(lpVault.userDeposit)} {lpVault.tokenName}
+          </Typography>
+        </Box>
+        <Button
+          onClick={handleClick}
+          disabled={disabled}
           sx={{
-            fontSize: '14px',
+            background: 'transparent',
             color: 'primary.light',
-            textTransform: 'uppercase',
-            marginLeft: '4px',
+            '&:hover': {
+              background: 'transparent',
+              borderStyle: 'none none solid none',
+              borderColor: 'primary.light',
+              borderRadius: '0px',
+            },
+            padding: '4px 1px',
+            fontSize: '14px',
+            lineHeight: '20px',
+            boxShadow: 'none',
+            borderStyle: 'none none none none',
+            borderRadius: '0px',
           }}
         >
-          {formatCurrency(lpVault.userDeposit)} {lpVault.tokenName}
-        </Typography>
-      </Box>
+          DEPOSIT
+        </Button>
+      </>
     );
   };
 
@@ -57,29 +80,6 @@ const MellowLPPosition: React.FunctionComponent<MellowLPPositionProps> = ({
       }}
     >
       {getPositionInfo()}
-
-      <Button
-        onClick={handleClick}
-        disabled={disabled}
-        sx={{
-          background: 'transparent',
-          color: 'primary.light',
-          '&:hover': {
-            background: 'transparent',
-            borderStyle: 'none none solid none',
-            borderColor: 'primary.light',
-            borderRadius: '0px',
-          },
-          padding: '4px 1px',
-          fontSize: '14px',
-          lineHeight: '20px',
-          boxShadow: 'none',
-          borderStyle: 'none none none none',
-          borderRadius: '0px',
-        }}
-      >
-        DEPOSIT
-      </Button>
     </Box>
   );
 };
