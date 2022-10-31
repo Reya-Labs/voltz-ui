1c2b40b7eee1d59b2190ce8076236986a677e023
ssbarbee <ssbarbee@users.noreply.github.com>
2022-10-31 14:49:09 +0100

    fix: styling for the boxes around Mellow

--- src/components/interface/MellowLPTable/components/MellowLPEntry.tsx
+++ src/components/interface/MellowLPTable/components/MellowLPEntry.tsx
@@ -22,53 +22,66 @@ const MellowLPEntry: React.FunctionComponent<MellowLPEntryProps> = ({
   return (
     <Box
       sx={{
-        backgroundColor: '#19152A',
-        padding: '16px',
-        maxWidth: '308px',
-        maxHeight: '408px',
-        width: '100%',
-        height: '100%',
-        filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
+        display: 'flex',
+        flexDirection: 'column',
       }}
     >
-      <Box sx={{ ...boxStyles }}>
-        <Typography variant="h6" sx={{ ...tagStyles }}>
-          LP OPTIMISER
-        </Typography>
-      </Box>
-
-      <Box sx={{ display: 'flex', marginLeft: '8px', marginTop: '16px', alignItems: 'center' }}>
-        <Mellow />
-
-        <Typography variant="h1" sx={titleStyles}>
-          MELLOW VAULT
-        </Typography>
-      </Box>
-
-      <Box>
-        <Typography variant="h6" sx={copyStyles}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec sit iaculis cras elit
-          dictum massa. Sit metus amet, tincidunt odio. Tristique sagittis, nisl in eu eu vestibulum
-          et. Ut sed mauris urna justo, dictumst molestie posuere.
-        </Typography>
-      </Box>
-
-      {lpVault.protocol !== '-' && (
-        <Box sx={{ marginLeft: '8px', marginTop: '16px' }}>
-          <PoolField
-            agent={Agents.LIQUIDITY_PROVIDER}
-            protocol={lpVault.protocol}
-            isBorrowing={false}
-            isBorrowTable={true}
-          />
+      <Box
+        sx={{
+          padding: (theme) => theme.spacing(4),
+          maxWidth: '308px',
+          maxHeight: '408px',
+          width: '100%',
+          height: '100%',
+          backgroundColor: '#19152A',
+          filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
+          borderRadius: '8px 8px 0px 0px',
+        }}
+      >
+        <Box sx={{ ...boxStyles }}>
+          <Typography variant="h6" sx={{ ...tagStyles }}>
+            LP OPTIMISER
+          </Typography>
         </Box>
-      )}
 
-      <Box sx={{ marginLeft: '8px', marginTop: '16px' }}>
-        <LPMellowVaultInfo lpVault={lpVault} />
+        <Box sx={{ display: 'flex', marginLeft: '8px', marginTop: '16px', alignItems: 'center' }}>
+          <Mellow />
+
+          <Typography variant="h1" sx={titleStyles}>
+            MELLOW VAULT
+          </Typography>
+        </Box>
+
+        <Box>
+          <Typography variant="h6" sx={copyStyles}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec sit iaculis cras elit
+            dictum massa. Sit metus amet, tincidunt odio. Tristique sagittis, nisl in eu eu
+            vestibulum et. Ut sed mauris urna justo, dictumst molestie posuere.
+          </Typography>
+        </Box>
+
+        {lpVault.protocol !== '-' && (
+          <Box sx={{ marginLeft: '8px', marginTop: '16px' }}>
+            <PoolField
+              agent={Agents.LIQUIDITY_PROVIDER}
+              protocol={lpVault.protocol}
+              isBorrowing={false}
+              isBorrowTable={true}
+            />
+          </Box>
+        )}
+
+        <Box sx={{ marginLeft: '8px', marginTop: '16px' }}>
+          <LPMellowVaultInfo lpVault={lpVault} />
+        </Box>
       </Box>
-
-      <Box sx={{ marginLeft: '8px', marginTop: '16px' }}>
+      <Box
+        sx={{
+          background: '#1E1A33',
+          padding: (theme) => theme.spacing(2, 4),
+          borderRadius: '0px 0px 8px 8px',
+        }}
+      >
         <MellowLPPosition lpVault={lpVault} handleClick={onSelectItem} disabled={disabled} />
       </Box>
     </Box>
