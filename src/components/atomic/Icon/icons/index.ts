ca2f1ced227802580e283335dcbf059448d4644c
ssbarbee <ssbarbee@users.noreply.github.com>
2022-10-31 15:39:05 +0100

    feat: add badges for OG season

--- src/components/atomic/Icon/icons/index.ts
+++ src/components/atomic/Icon/icons/index.ts
@@ -20,6 +20,7 @@ export { ReactComponent as TokenLido } from './tokens/lido.svg';
 export { ReactComponent as TokenUsdc } from './tokens/usdc.svg';
 export { ReactComponent as TokenUsdt } from './tokens/usdt.svg';
 
+// season1 badges
 export { ReactComponent as BeWaterMyFriend } from './badges/season1/beWaterMyFriend.svg';
 export { ReactComponent as DegenStuff } from './badges/season1/degenStuff.svg';
 export { ReactComponent as DeltaDegen } from './badges/season1/deltaDegen.svg';
@@ -37,3 +38,22 @@ export { ReactComponent as TopTrader } from './badges/season1/topTrader.svg';
 export { ReactComponent as Yikes } from './badges/season1/yikes.svg';
 export { ReactComponent as SushiRoll } from './badges/season1/sushiRoll.svg';
 export { ReactComponent as WaterHose } from './badges/season1/waterHose.svg';
+
+// OG badges
+export { ReactComponent as OGBeWaterMyFriend } from './badges/og/ogBeWaterMyFriend.svg';
+export { ReactComponent as OGDegenStuff } from './badges/og/ogDegenStuff.svg';
+export { ReactComponent as OGDeltaDegen } from './badges/og/ogDeltaDegen.svg';
+export { ReactComponent as OGDryIce } from './badges/og/ogDryIce.svg';
+export { ReactComponent as OGIrsConnoisseur } from './badges/og/ogIrsConnoisseur.svg';
+export { ReactComponent as OGLeverageCrowbar } from './badges/og/ogLeverageCrowbar.svg';
+export { ReactComponent as OGLpoor } from './badges/og/ogLpoor.svg';
+export { ReactComponent as OGMaxBidding } from './badges/og/ogMaxBidding.svg';
+export { ReactComponent as OGMoneyMoneyMoney } from './badges/og/ogMoneyMoneyMoney.svg';
+export { ReactComponent as OGNoClaimedBadges } from './badges/og/ogNoClaimedBadges.svg';
+export { ReactComponent as OGFixedTrader } from './badges/og/ogFixedTrader.svg';
+export { ReactComponent as OGOkBoomer } from './badges/og/ogOkBoomer.svg';
+export { ReactComponent as OGRainmaker } from './badges/og/ogRainmaker.svg';
+export { ReactComponent as OGTopTrader } from './badges/og/ogTopTrader.svg';
+export { ReactComponent as OGYikes } from './badges/og/ogYikes.svg';
+export { ReactComponent as OGSushiRoll } from './badges/og/ogSushiRoll.svg';
+export { ReactComponent as OGWaterHose } from './badges/og/ogWaterHose.svg';
