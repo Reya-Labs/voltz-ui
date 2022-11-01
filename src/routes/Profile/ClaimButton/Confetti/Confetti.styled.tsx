7eb27f889eac7e24d71ab3c1e5e12653db0b7eea
ssbarbee <ssbarbee@users.noreply.github.com>
2022-11-01 15:21:24 +0100

    fix: improve confetti

--- src/routes/Profile/ClaimButton/Confetti/Confetti.styled.tsx
+++ src/routes/Profile/ClaimButton/Confetti/Confetti.styled.tsx
@@ -7,32 +7,32 @@ export const Container = styled('div')`
     position: absolute;
     display: block;
     left: 50%;
-    top: 0;
+    top: 50%;
     width: 5px;
-    height: 10px;
+    height: 7px;
     background: red;
     opacity: 0;
   }
   &:hover i:nth-of-type(1) {
-    transform: translate3d(-82px, 100px, 0) rotate(297deg);
+    transform: translate3d(-82px, 60px, 0) rotate(297deg);
     background: #8000ff;
     animation: bang 700ms ease-out forwards;
     opacity: 0;
   }
   &:hover i:nth-of-type(2) {
-    transform: translate3d(-67px, -89px, 0) rotate(64deg);
+    transform: translate3d(-67px, -49px, 0) rotate(64deg);
     background: #ffae00;
     animation: bang 700ms ease-out forwards;
     opacity: 0;
   }
   &:hover i:nth-of-type(3) {
-    transform: translate3d(-89px, 70px, 0) rotate(45deg);
+    transform: translate3d(-89px, 30px, 0) rotate(45deg);
     background: #ff2600;
     animation: bang 700ms ease-out forwards;
     opacity: 0;
   }
   &:hover i:nth-of-type(4) {
-    transform: translate3d(-8px, 47px, 0) rotate(245deg);
+    transform: translate3d(-8px, 7px, 0) rotate(245deg);
     background: #77ff00;
     animation: bang 700ms ease-out forwards;
     opacity: 0;
@@ -50,13 +50,13 @@ export const Container = styled('div')`
     opacity: 0;
   }
   &:hover i:nth-of-type(7) {
-    transform: translate3d(59px, 95px, 0) rotate(180deg);
+    transform: translate3d(59px, 55px, 0) rotate(180deg);
     background: #0059ff;
     animation: bang 700ms ease-out forwards;
     opacity: 0;
   }
   &:hover i:nth-of-type(8) {
-    transform: translate3d(-76px, -82px, 0) rotate(272deg);
+    transform: translate3d(-76px, -42px, 0) rotate(272deg);
     background: #0033ff;
     animation: bang 700ms ease-out forwards;
     opacity: 0;
@@ -68,7 +68,7 @@ export const Container = styled('div')`
     opacity: 0;
   }
   &:hover i:nth-of-type(10) {
-    transform: translate3d(-53px, 47px, 0) rotate(103deg);
+    transform: translate3d(-53px, 7px, 0) rotate(103deg);
     background: #00fff7;
     animation: bang 700ms ease-out forwards;
     opacity: 0;
@@ -80,7 +80,7 @@ export const Container = styled('div')`
     opacity: 0;
   }
   &:hover i:nth-of-type(12) {
-    transform: translate3d(34px, -74px, 0) rotate(247deg);
+    transform: translate3d(34px, -34px, 0) rotate(247deg);
     background: #ffa200;
     animation: bang 700ms ease-out forwards;
     opacity: 0;
@@ -110,25 +110,25 @@ export const Container = styled('div')`
     opacity: 0;
   }
   &:hover i:nth-of-type(17) {
-    transform: translate3d(-82px, -48px, 0) rotate(353deg);
+    transform: translate3d(-82px, -8px, 0) rotate(353deg);
     background: #ff0040;
     animation: bang 700ms ease-out forwards;
     opacity: 0;
   }
   &:hover i:nth-of-type(18) {
-    transform: translate3d(-34px, -80px, 0) rotate(181deg);
+    transform: translate3d(-34px, -40px, 0) rotate(181deg);
     background: #ff004d;
     animation: bang 700ms ease-out forwards;
     opacity: 0;
   }
   &:hover i:nth-of-type(19) {
-    transform: translate3d(32px, -88px, 0) rotate(77deg);
+    transform: translate3d(32px, -48px, 0) rotate(77deg);
     background: #ff008c;
     animation: bang 700ms ease-out forwards;
     opacity: 0;
   }
   &:hover i:nth-of-type(20) {
-    transform: translate3d(85px, -82px, 0) rotate(344deg);
+    transform: translate3d(85px, -42px, 0) rotate(344deg);
     background: #ff006a;
     animation: bang 700ms ease-out forwards;
     opacity: 0;
--- src/routes/Profile/ClaimButton/Confetti/Confetti.tsx
+++ src/routes/Profile/ClaimButton/Confetti/Confetti.tsx
@@ -2,59 +2,14 @@ import React, { FunctionComponent } from 'react';
 import { Container } from './Confetti.styled';
 
 export const Confetti: FunctionComponent = ({ children }) => {
+  const leafElements = React.useMemo(
+    () => Array.from({ length: 20 }, (_, index) => <i key={index} />),
+    [],
+  );
   return (
     <Container>
       {children}
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
+      {leafElements}
     </Container>
   );
 };
