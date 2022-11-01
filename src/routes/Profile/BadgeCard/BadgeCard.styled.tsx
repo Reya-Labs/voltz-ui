30215cb9d3a137a0b502b768dde3469a9d01c966
ssbarbee <ssbarbee@users.noreply.github.com>
2022-11-01 21:42:15 +0100

    fix: improve styling a bit

--- src/routes/Profile/BadgeCard/BadgeCard.styled.tsx
+++ src/routes/Profile/BadgeCard/BadgeCard.styled.tsx
@@ -6,10 +6,16 @@ import { colors } from '@theme';
 
 export const Container = styled(Box)`
   background-color: #19152a;
-  border-radius: 4px;
+  border-radius: 8px;
   padding: ${({ theme }) => theme.spacing(4)};
   display: flex;
   flex-direction: column;
+  transition: box-shadow 0.25s ease-out 0s, border 0.25s ease-out 0s;
+  box-sizing: border-box;
+
+  &:hover {
+    box-shadow: rgb(62 115 196 / 40%) 0px 0px 35px;
+  }
 `;
 
 export const BadgePillBox = styled(Box)`
