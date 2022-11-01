3f41ebdf72585b1430245724ebba9575922c71f8
ssbarbee <ssbarbee@users.noreply.github.com>
2022-11-01 10:56:04 +0100

    feat/lp opt fix (#313)
    * feat: removing lev slider and adding buttons
    
    * fix: correct button style
    
    * fix: refactor array and rename options list
    
    * fix: change default leverage to 100x to match buttons
    
    * fix: remove unsued isDisable variable & move style to style.ts
    
    * test: add small testcase for elide address (#294)
    
    * feat: vault page styled
    
    * feat: styling done
    
    * feat: mock smart contract calls
    
    * feat: submission state handling
    
    * feat: working checkpoint
    
    * flows working
    
    * feat: working checkpoint
    
    * feat: automated cap info
    
    * feat: goerli setup
    
    * feat: added env variables
    
    * feat: trigger build
    
    * feat: checked before PR
    
    * fix: nav ecosystem
    
    * fix: removed unused variable
    
    * feat: resolved PR review comments
    
    * fix: formatted files
    
    * feature/introduce styled (#296)
    
    * feat: add styling via styled components
    
    * fix: elideAddress in profile
    
    * feat: added tests for address formatter
    
    * fix: prettier command run
    
    * fix: small fix in protocol
    
    * feat: add pipeline prettier check
    
    * test: adding ugly file
    
    * Revert "test: adding ugly file"
    
    This reverts commit 9932958ac8eb086dbea0b5ecf4a652d41bb25dbe.
    
    * feat: eslint check
    
    * fix: removed unused variables
    
    * chore: fix prettier
    
    * chore: fix prettier + lint
    
    * fix: prevent risky wallet error msg being overriden
    
    * feat: added support for ETH
    
    * feat: increased gas limit for deposit
    
    * feat: added lower bound of 2mil gas limit on mellow
    
    * fix: nav link improvements + back button
    
    * feat: send user's address as GA event param
    
    * feat: add user address as GA push function param
    
    * fix: card background & hide withdraw and lp opt tag
    
    * fix: styling for the boxes around Mellow
    
    * fix: added loader indicator for the position and vault info
    
    * fix: add copies and fix card height
    
    * fix: margin and padding using theme
    
    * fix: copies
    
    * feat: route from LP to Products
    
    * fix: prettier
    
    * refactor: move to folder season1
    
    * feat: add badges for OG season
    
    * feat: support OG badges in profile
    
    * feat: add util doNothing
    
    * feat: add support for toggle of season
    
    * feat: flag feature under REACT_APP_COMMUNITY_P2
    
    * test: correct failing tests due to incorrect format
    
    * fix: reset ecosystem on every location change when not in PAGE mode
    
    * fix: make ecosystem env more granular for products page
    
    Co-authored-by: Soleiman Anwary <soleiman.anwary@gmail.com>
    Co-authored-by: Sol <35786191+Sol3iman@users.noreply.github.com>
    Co-authored-by: tudor47 <tudorcostin7@gmail.com>
    Co-authored-by: Ioana Roceanu <ioanaroceanu@gmail.com>
    Co-authored-by: Costin-Razvan Tudor <39092730+0xZenus@users.noreply.github.com>
    Co-authored-by: Artur Begyan <36239705+arturbeg@users.noreply.github.com>
    Co-authored-by: Artur Begyan <arturbegyan98@gmail.com>

--- src/components/interface/Nav/Nav.tsx
+++ src/components/interface/Nav/Nav.tsx
@@ -74,30 +74,30 @@ const Nav: React.FunctionComponent = () => (
       </NavLink>
     )}
 
-      <NavLink
-        isNew={true}
-        subLinks={
-          process.env.REACT_APP_ECOSYSTEM && process.env.REACT_APP_ECOSYSTEM !== `UNPROVIDED` ?
-          [
-          {
-            text: 'PRODUCTS',
-            link: `/${routes.PRODUCTS}`,
-          },
-          {
-            text: 'FIXED BORROW',
-            link: `/${routes.BORROW_POS}`,
-          },
-        ] : 
-        [
-          {
-            text: 'FIXED BORROW',
-            link: `/${routes.BORROW_POS}`,
-          },
-        ]
+    <NavLink
+      isNew={true}
+      subLinks={
+        process.env.REACT_APP_ECOSYSTEM && process.env.REACT_APP_ECOSYSTEM !== `UNPROVIDED`
+          ? [
+              {
+                text: 'PRODUCTS',
+                link: `/${routes.PRODUCTS}`,
+              },
+              {
+                text: 'FIXED BORROW',
+                link: `/${routes.BORROW_POS}`,
+              },
+            ]
+          : [
+              {
+                text: 'FIXED BORROW',
+                link: `/${routes.BORROW_POS}`,
+              },
+            ]
       }
-      >
-        Ecosystem
-      </NavLink>
+    >
+      Ecosystem
+    </NavLink>
   </Box>
 );
 
