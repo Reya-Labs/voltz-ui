9291fc1979259f9502678c935420a855029fd017
ssbarbee <ssbarbee@users.noreply.github.com>
2022-11-01 21:36:19 +0100

    feat: add ClaimButton component

---
+++ src/routes/Profile/ClaimButton/ClaimButton.styled.tsx
@@ -0,0 +1,28 @@
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import { colors } from '@theme';
+
+export const ClaimButton = styled(Button)<{ stretch: boolean; disabled: boolean }>`
+  font-family: 'PixelOperatorMono', monospace;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  border-radius: 8px;
+  background: #1e1a33;
+  width: 100%;
+  ${({ stretch }) => (stretch ? 'display: flex' : '')};
+  ${({ stretch }) => (stretch ? 'justify-content: space-between' : '')};
+  padding: ${({ theme }) => theme.spacing(2, 4)};
+  color: ${({ disabled }) =>
+    disabled ? colors.lavenderWeb.darken015 : colors.skyBlueCrayola.base};
+`;
+
+export const TickWrapper = styled('div')`
+  margin-left: ${({ theme }) => theme.spacing(1)};
+`;
+
+export const ClaimedAtTypography = styled('span')`
+  color: ${colors.skyBlueCrayola.base};
+  margin-left: ${({ theme }) => theme.spacing(1)};
+`;
