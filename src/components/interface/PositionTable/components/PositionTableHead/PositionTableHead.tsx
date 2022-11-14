f052c89dd7a3eb254c88e1f7789486e64d31d734
ssbarbee <ssbarbee@users.noreply.github.com>
2022-11-14 16:09:32 +0100

    refactor: component PositionTableHead props fix

--- src/components/interface/PositionTable/components/PositionTableHead/PositionTableHead.tsx
+++ src/components/interface/PositionTable/components/PositionTableHead/PositionTableHead.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import { colors, SystemStyleObject, Theme } from '@theme';
-import { AugmentedAMM, formatCurrency, formatNumber } from '@utilities';
+import { formatCurrency, formatNumber } from '@utilities';
 import { Button, getPositionBadgeVariant, PositionBadge, Typography } from '@components/atomic';
 import { BulletLabel, getHealthTextColor, HealthFactorText } from '@components/composite';
 import { isUndefined } from 'lodash';
-import { PositionInfo } from '@voltz-protocol/v1-sdk/dist/types/entities';
 import { useAgent } from '@hooks';
 import { Agents } from '@contexts';
 import { ReactComponent as EditIcon } from './editPosition.svg';
 
 export type PositionTableHeadProps = {
-  currencyCode?: string;
-  currencySymbol?: string;
-  isFCM?: boolean;
-  feesPositive?: boolean;
+  currencyCode: string;
+  currencySymbol: string;
+  feesPositive: boolean;
   isSettled: boolean;
   positionType: number;
-  info: PositionInfo | undefined;
   onRollover: () => void;
   onSettle: () => void;
-  rolloverAmm?: AugmentedAMM;
-  gaButtonId?: string;
+  rolloverAvailable: boolean;
+  gaButtonId: string;
   onSelect?: (mode: 'margin' | 'liquidity' | 'notional') => void;
+  beforeMaturity?: boolean;
+  healthFactor?: number;
+  fixedRateHealthFactor?: number;
+  fixedApr?: number;
+  fees?: number;
 };
 
 const containerStyles: SystemStyleObject<Theme> = {
@@ -43,38 +45,36 @@ const labelStyles: SystemStyleObject<Theme> = {
 const PositionTableHead: React.FunctionComponent<PositionTableHeadProps> = ({
   currencyCode = '',
   currencySymbol = '',
-  isFCM = false,
   feesPositive = true,
   isSettled,
   positionType,
-  info,
   onRollover,
   onSettle,
-  rolloverAmm,
+  rolloverAvailable,
   gaButtonId,
   onSelect,
+  beforeMaturity,
+  healthFactor,
+  fixedRateHealthFactor,
+  fixedApr,
+  fees: feesProp,
 }) => {
   const { agent } = useAgent();
-  const beforeMaturity = info?.beforeMaturity;
-  const healthFactor = info?.healthFactor;
-  const fixedRateHealthFactor = info?.fixedRateHealthFactor;
-  const currentFixedRate = agent === Agents.LIQUIDITY_PROVIDER ? info?.fixedApr : undefined;
-  const fees = agent === Agents.LIQUIDITY_PROVIDER ? info?.fees : undefined;
+  const currentFixedRate = agent === Agents.LIQUIDITY_PROVIDER ? fixedApr : undefined;
+  const fees = agent === Agents.LIQUIDITY_PROVIDER ? feesProp : undefined;
 
   const getTextColor = (positive: boolean) => {
     return positive ? colors.vzCustomGreen1.base : colors.vzCustomRed1.base;
   };
 
   const handleEditNotional = () => {
-    if (onSelect) onSelect('notional');
+    onSelect && onSelect('notional');
   };
 
   return (
     <Box sx={containerStyles}>
       <Box sx={{ display: 'flex' }}>
         <PositionBadge variant={getPositionBadgeVariant(positionType)} />
-
-        {isFCM && <PositionBadge variant="FC" sx={{ marginLeft: (theme) => theme.spacing(2) }} />}
 
         {!isUndefined(fees) && (
           <Box
@@ -153,7 +153,7 @@ const PositionTableHead: React.FunctionComponent<PositionTableHeadProps> = ({
             >
               Settle
             </Button>
-            {rolloverAmm && !isFCM && (
+            {rolloverAvailable && (
               <Button
                 variant={
                   positionType === 1 ? 'rollover1' : positionType === 2 ? 'rollover2' : 'rollover3'
