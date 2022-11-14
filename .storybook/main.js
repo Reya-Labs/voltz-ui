174ffa59b2fdbf95fccbb30a7180ffa60fae0f0b
ssbarbee <ssbarbee@users.noreply.github.com>
2022-11-14 18:28:16 +0100

    feat: allow for semantically releasing UI version

--- .storybook/main.js
+++ .storybook/main.js
@@ -38,17 +38,6 @@ module.exports = {
     config.resolve.alias['@utilities'] = path.resolve(path.dirname(__dirname), 'src/utilities');
     config.resolve.alias['@routes'] = path.resolve(path.dirname(__dirname), 'src/routes');
     config.resolve.alias['@factories'] = path.resolve(path.dirname(__dirname), 'src/factories');
-    config.resolve.fallback = {
-      buffer: require.resolve('buffer'),
-      crypto: require.resolve('crypto-browserify'),
-      stream: require.resolve('stream-browserify'),
-      assert: require.resolve('assert'),
-      http: require.resolve('stream-http'),
-      https: require.resolve('https-browserify'),
-      os: require.resolve('os-browserify'),
-      url: require.resolve('url'),
-      ...(config.resolve.fallback || {}),
-    };
     return config;
   },
   features: {
