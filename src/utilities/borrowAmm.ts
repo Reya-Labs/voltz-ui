664dafec528a39c90446f46995c557f4a4df3f40
ssbarbee <ssbarbee@users.noreply.github.com>
2022-11-14 16:03:00 +0100

    refactor: pass id to fn

--- src/utilities/borrowAmm.ts
+++ src/utilities/borrowAmm.ts
@@ -1,18 +1,14 @@
-import AugmentedBorrowAMM from './augmentedBorrowAmm';
 import { Position } from '@voltz-protocol/v1-sdk/dist/types/entities';
 
 /**
  * Returns the current position that the user has for the given amm
  * @param positions - the array of positions the user has
- * @param selectedAmm - the selected amm
+ * @param selectedAmmId - the selected amm id
  */
-export const findCurrentBorrowPosition = (
-  positions: Position[],
-  selectedAmm: AugmentedBorrowAMM,
-) => {
+export const findCurrentBorrowPosition = (positions: Position[], selectedAmmId: string) => {
   return (positions || []).find((p) => {
     return (
-      p.amm.id === selectedAmm.id &&
+      p.amm.id === selectedAmmId &&
       p.positionType === 2 &&
       p.tickLower === -69000 &&
       p.tickUpper === 69060 &&
