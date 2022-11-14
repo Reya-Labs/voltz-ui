c121037b7cbca303d62691c28f7e181816f7931b
ssbarbee <ssbarbee@users.noreply.github.com>
2022-11-14 18:28:16 +0100

    feat: allow for semantically releasing UI version

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -275,16 +275,6 @@ module.exports = function (webpackEnv) {
           babelRuntimeRegenerator,
         ]),
       ],
-      fallback: {
-        buffer: require.resolve('buffer'),
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
-        assert: require.resolve('assert'),
-        http: require.resolve('stream-http'),
-        https: require.resolve('https-browserify'),
-        os: require.resolve('os-browserify'),
-        url: require.resolve('url'),
-      },
     },
     module: {
       strictExportPresence: true,
