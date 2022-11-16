f5eeb7bce719eb69572eb5a18963b74bd9ed855f
tudor47 <tudorcostin7@gmail.com>
2022-11-17 01:07:34 +0200

    feat: removed undefined where unnecessary

--- src/components/interface/BorrowTable/utilities/mapAmmToAmmTableDatum.ts
+++ src/components/interface/BorrowTable/utilities/mapAmmToAmmTableDatum.ts
@@ -2,17 +2,14 @@ import { BorrowAMMTableDatum } from '../types';
 
 import { BorrowAMM } from '@voltz-protocol/v1-sdk';
 
-const mapAmmToAmmTableDatum = ({ id, amm }: BorrowAMM): BorrowAMMTableDatum | undefined => {
-  if (amm) {
-    return {
-      id,
-      protocol: amm.protocol,
-      underlyingTokenName: amm.underlyingToken.name,
-      startDate: amm.startDateTime,
-      endDate: amm.endDateTime,
-    };
-  }
-  return undefined;
+const mapAmmToAmmTableDatum = ({ id, amm }: BorrowAMM): BorrowAMMTableDatum => {
+  return {
+    id,
+    protocol: amm.protocol,
+    underlyingTokenName: amm.underlyingToken.name,
+    startDate: amm.startDateTime,
+    endDate: amm.endDateTime,
+  };
 };
 
 export default mapAmmToAmmTableDatum;
