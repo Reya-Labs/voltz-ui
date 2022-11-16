ac6f7cc265000961e35cbcc8db01937f1e705cf6
ssbarbee <ssbarbee@users.noreply.github.com>
2022-11-16 11:17:54 +0100

    refactor: rename ClaimNotificationSection to NotificationSection

--- src/routes/Profile/NotificationSection/NotificationSection.styled.tsx
+++ src/routes/Profile/NotificationSection/NotificationSection.styled.tsx
@@ -1,11 +1,11 @@
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 
-export const ClaimNotificationBox = styled(Box)`
+export const NotificationBox = styled(Box)`
   padding: ${({ theme }) => theme.spacing(2, 4)};
 `;
 
-export const ClaimNotificationContainer = styled(Box)`
+export const NotificationsContainer = styled(Box)`
   background-color: #19152a;
   display: flex;
   align-items: center;
@@ -14,6 +14,6 @@ export const ClaimNotificationContainer = styled(Box)`
   border-radius: 8px;
 `;
 
-export const ClaimButtonBox = styled(Box)`
+export const ButtonBox = styled(Box)`
   width: 150px;
 `;
