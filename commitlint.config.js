0c561dbf645411b2bb19b857e331a3ace1f8cbd8
CyclopsRex <cyclopsrex@voltz.xyz>
2022-11-17 23:02:46 +0000

    chore: disable line length check in commit msg body

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,15 +2,10 @@ module.exports = {
   parserPreset: 'conventional-changelog-conventionalcommits',
   rules: {
     'body-leading-blank': [1, 'always'],
-    'body-max-line-length': [2, 'always', 100],
     'footer-leading-blank': [1, 'always'],
     'footer-max-line-length': [2, 'always', 100],
     'header-max-length': [2, 'always', 100],
-    'subject-case': [
-      2,
-      'never',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
-    ],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'type-case': [2, 'always', 'lower-case'],
@@ -60,8 +55,7 @@ module.exports = {
             emoji: '💎',
           },
           refactor: {
-            description:
-              'A code change that neither fixes a bug nor adds a feature',
+            description: 'A code change that neither fixes a bug nor adds a feature',
             title: 'Code Refactoring',
             emoji: '📦',
           },
@@ -100,12 +94,10 @@ module.exports = {
         },
       },
       scope: {
-        description:
-          'What is the scope of this change (e.g. component or file name)?',
+        description: 'What is the scope of this change (e.g. component or file name)?',
       },
       subject: {
-        description:
-          'Write a short, imperative tense description of the change.',
+        description: 'Write a short, imperative tense description of the change.',
       },
       body: {
         description: 'Provide a longer description of the change.',
