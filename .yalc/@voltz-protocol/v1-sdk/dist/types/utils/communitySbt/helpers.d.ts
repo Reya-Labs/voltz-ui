08074d4ee54b8b368348286e2ea624e6c18b4656
ssbarbee <ssbarbee@users.noreply.github.com>
2022-11-17 08:15:03 +0100

    chore: bump sdk

---
+++ .yalc/@voltz-protocol/v1-sdk/dist/types/utils/communitySbt/helpers.d.ts
@@ -0,0 +1,6 @@
+import { Bytes } from "ethers";
+export declare function getBadgeTypeFromMetadataUri(metadataURI: string): number;
+export declare function decodeBadgeType(input: Bytes): number;
+export declare function decodeMultipleBadgeTypes(input: Bytes): number[];
+export declare function getEtherscanURL(network: string, apiKey: string, userAddress: string): string;
+//# sourceMappingURL=helpers.d.ts.map
