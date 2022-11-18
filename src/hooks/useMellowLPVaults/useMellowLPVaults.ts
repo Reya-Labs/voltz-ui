71854a2113eda3bbddab45d08e41743443cbb4c8
tudor47 <tudorcostin7@gmail.com>
2022-11-18 12:47:58 +0200

    fix: prettier

--- src/hooks/useMellowLPVaults/useMellowLPVaults.ts
+++ src/hooks/useMellowLPVaults/useMellowLPVaults.ts
@@ -5,16 +5,20 @@ import { getConfig } from './config';
 const useMellowLPVaults = (): MellowLpVault[] => {
   const config = getConfig();
 
-  const lpVaults = useMemo(() => config.MELLOW_VAULTS.map(
-    (item) =>
-      new MellowLpVault({
-        ethWrapperAddress: config.MELLOW_ETH_WRAPPER,
-        voltzVaultAddress: item.voltzVault,
-        erc20RootVaultAddress: item.erc20RootVault,
-        erc20RootVaultGovernanceAddress: item.erc20RootVaultGovernance,
-        provider: config.PROVIDER,
-      }),
-  ), []);
+  const lpVaults = useMemo(
+    () =>
+      config.MELLOW_VAULTS.map(
+        (item) =>
+          new MellowLpVault({
+            ethWrapperAddress: config.MELLOW_ETH_WRAPPER,
+            voltzVaultAddress: item.voltzVault,
+            erc20RootVaultAddress: item.erc20RootVault,
+            erc20RootVaultGovernanceAddress: item.erc20RootVaultGovernance,
+            provider: config.PROVIDER,
+          }),
+      ),
+    [],
+  );
 
   return lpVaults;
 };
