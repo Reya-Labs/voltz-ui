a07fac7a7e69e10073d439eef35b414e9bdf1691
ssbarbee <ssbarbee@users.noreply.github.com>
2022-11-21 13:26:13 +0100

    feat: remove yalc related stuff, use NPM for sdk-v1

--- .yalc/@voltz-protocol/v1-sdk/dist/types/typechain-sbt/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type * as contracts from "./contracts";
-export type { contracts };
-export * as factories from "./factories";
-export type { CommunitySBT } from "./contracts/CommunitySBT";
-export { CommunitySBT__factory } from "./factories/contracts/CommunitySBT__factory";
-export type { IERC5192 } from "./contracts/IERC5192";
-export { IERC5192__factory } from "./factories/contracts/IERC5192__factory";
-//# sourceMappingURL=index.d.ts.map
--- .yalc/@voltz-protocol/v1-sdk/dist/types/utils/communitySbt/getSubgraphLeaves.d.ts
+++
@@ -1,3 +0,0 @@
-import { LeafInfo } from '../../entities/communitySbt';
-export declare function createLeaves(seasonStart: number, seasonEnd: number, subgraphUrl: string): Promise<Array<LeafInfo>>;
-//# sourceMappingURL=getSubgraphLeaves.d.ts.map
