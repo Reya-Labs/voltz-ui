55cb750a40710322c4b1fc92ecea0d1b34c04745
ssbarbee <ssbarbee@users.noreply.github.com>
2022-11-22 11:24:37 +0100

    feat: add learn more link

--- src/routes/Ecosystem/EcosystemHeader/EcosystemHeader.tsx
+++ src/routes/Ecosystem/EcosystemHeader/EcosystemHeader.tsx
@@ -1,21 +1,20 @@
-import { Box } from '@mui/system';
-import { Panel, Typography } from '../../../components/atomic';
-import { titleStyles, copyStyles } from './styles';
+import {
+  DescriptionTypography,
+  EcosystemHeaderBox,
+  LearnMoreLink,
+  TitleTypography,
+} from './EcosystemHeader.styled';
+import React from 'react';
 
-const EcosystemHeader = () => {
-  return (
-    <Panel sx={{ padding: 0, background: 'transparent' }}>
-      <Box sx={{ maxWidth: '743px' }}>
-        <Typography variant="h1" sx={titleStyles}>
-          LP OPTIMIZER VAULTS
-        </Typography>
-        <Typography variant="body1" sx={copyStyles}>
-          The Voltz-Mellow Vaults run automated strategies, optimizing deposits for high LP fees
-          while taking away the complex math, empowering every user to become a Voltz LP.
-        </Typography>
-      </Box>
-    </Panel>
-  );
-};
+const EcosystemHeader: React.FunctionComponent = React.memo(() => (
+  <EcosystemHeaderBox>
+    <TitleTypography variant="h1">LP OPTIMIZER VAULTS</TitleTypography>
+    <DescriptionTypography variant="body1">
+      The Voltz-Mellow Vaults run automated strategies, optimizing deposits for high LP fees while
+      taking away the complex math, empowering every user to become a Voltz LP.
+    </DescriptionTypography>
+    <LearnMoreLink to="https://voltz.xyz">LEARN MORE</LearnMoreLink>
+  </EcosystemHeaderBox>
+));
 
 export default EcosystemHeader;
