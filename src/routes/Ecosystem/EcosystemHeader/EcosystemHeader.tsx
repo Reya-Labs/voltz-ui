f9ca15fa49e127f5dbb233322dfcc8db10f9336d
tudor47 <tudorcostin7@gmail.com>
2022-11-22 13:17:11 +0200

    feat: optimiser

--- src/routes/Ecosystem/EcosystemHeader/EcosystemHeader.tsx
+++ src/routes/Ecosystem/EcosystemHeader/EcosystemHeader.tsx
@@ -8,7 +8,7 @@ import React from 'react';
 
 const EcosystemHeader: React.FunctionComponent = React.memo(() => (
   <EcosystemHeaderBox>
-    <TitleTypography variant="h1">LP OPTIMIZER VAULTS</TitleTypography>
+    <TitleTypography variant="h1">LP OPTIMISER VAULTS</TitleTypography>
     <DescriptionTypography variant="body1">
       The Voltz-Mellow Vaults run automated strategies, optimizing deposits for high LP fees while
       taking away the complex math, empowering every user to become a Voltz LP.
