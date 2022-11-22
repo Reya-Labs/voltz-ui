4a6d46db43d3cf06179d4cd76a6b37d88e238c19
ssbarbee <ssbarbee@users.noreply.github.com>
2022-11-22 10:59:50 +0100

    test: fix broken tests due to missing HashRouter wrapper

--- src/routes/ProfileV1/ProfilePageWalletConnected/ProfilePageWalletConnected.test.tsx
+++ src/routes/ProfileV1/ProfilePageWalletConnected/ProfilePageWalletConnected.test.tsx
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { composeStories } from '@storybook/testing-react';
 
 import * as stories from './ProfilePageWalletConnected.stories';
+import { HashRouter } from "react-router-dom";
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
@@ -13,7 +14,11 @@ const { Default } = composeStories(stories);
 
 describe('<ProfilePageWalletConnected />', () => {
   it('should render proper UI when claimed badges present', () => {
-    render(<Default />);
+    render(
+      <HashRouter>
+        <Default />
+      </HashRouter>
+    );
 
     expect(screen.getByText('WELCOME TO YOUR PROFILE')).not.toBeNull();
     expect(screen.getByText('0XB01F...C970')).not.toBeNull();
