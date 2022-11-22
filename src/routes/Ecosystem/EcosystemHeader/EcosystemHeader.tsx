2f75709fd3fe8ad3f9e6b3bd7f3b30654f5b4f02
ssbarbee <ssbarbee@users.noreply.github.com>
2022-11-22 15:05:58 +0100

    fix: update learn more link

--- src/routes/Ecosystem/EcosystemHeader/EcosystemHeader.tsx
+++ src/routes/Ecosystem/EcosystemHeader/EcosystemHeader.tsx
@@ -13,7 +13,10 @@ const EcosystemHeader: React.FunctionComponent = React.memo(() => (
       The Voltz-Mellow Vaults run automated strategies, optimizing deposits for high LP fees while
       taking away the complex math, empowering every user to become a Voltz LP.
     </DescriptionTypography>
-    <LearnMoreLink href="https://voltz.xyz" target="_blank">
+    <LearnMoreLink
+      href="https://www.voltz.xyz/resource-centre/optimizing-the-optimizooor-the-new-mellow-voltz-eth-and-stablecoin-lp-optimizers"
+      target="_blank"
+    >
       LEARN MORE
     </LearnMoreLink>
   </EcosystemHeaderBox>
