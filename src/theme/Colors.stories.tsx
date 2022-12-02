2dfc131ef72e4602d22dd70e115afa196f125bf2
ssbarbee <ssbarbee@users.noreply.github.com>
2022-12-02 09:46:04 +0100

    refactor: prettier

--- src/theme/Colors.stories.tsx
+++ src/theme/Colors.stories.tsx
@@ -1,4 +1,4 @@
-import { ComponentMeta,ComponentStory } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 
 import colors from './colors';
