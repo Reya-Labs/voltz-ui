f8625e88327a779eeb9da7c8fdea02b5f67c9708
ssbarbee <ssbarbee@users.noreply.github.com>
2022-12-05 10:02:09 +0100

    feat: allow for splitting between new form and old

---
+++ src/routes/LPOptimisers/deprecated/EcosystemDepositV1/EcosystemDeposit.styled.tsx
@@ -0,0 +1,9 @@
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+
+export const ConnectedMellowBox = styled(Box)`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  backdrop-filter: blur(8px);
+`;
