9c3153455cd7d0d77ed27b5a80ad2de417f10748
ssbarbee <ssbarbee@users.noreply.github.com>
2022-12-06 14:11:40 +0100

    refactor: improve theme object

--- src/theme/types.ts
+++ src/theme/types.ts
@@ -54,29 +54,6 @@ export type BaseColors =
   | 'ultramarineBlue7'
   | 'ultramarineBlue8';
 
-type ColorSet = {
-  base: string;
-  darken010: string;
-  darken015: string;
-  darken020: string;
-  darken025: string;
-  darken030: string;
-  darken035: string;
-  darken040: string;
-  darken045: string;
-  darken050: string;
-};
-declare module '@mui/material/styles' {
-  interface Palette {
-    tertiary: Palette['primary'];
-  }
-  interface PaletteOptions {
-    tertiary: PaletteOptions['primary'];
-  }
-  interface PaletteColor extends ColorSet {}
-  interface SimplePaletteColorOptions extends ColorSet {}
-}
-
 declare module '@mui/material/Button' {
   interface ButtonPropsVariantOverrides {
     dark: true;
