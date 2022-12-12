7e985a89c4f7966f443e530699bb2ab20139afa2
ssbarbee <ssbarbee@users.noreply.github.com>
2022-12-12 10:01:18 +0100

    chore: typos

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -429,7 +429,7 @@ module.exports = function (webpackEnv) {
               // Exclude `js` files to keep "css" loader working as it injects
               // its runtime that would otherwise be processed through "file" loader.
               // Also exclude `html` and `json` extensions so they get processed
-              // by webpacks internal loaders.
+              // by Webpack internal loaders.
               exclude: [/^$/, /\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
               type: 'asset/resource',
             },
