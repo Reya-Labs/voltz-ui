7f234c4d2ef55a257a445f5128013cd8ff15d41e
ssbarbee <ssbarbee@users.noreply.github.com>
2022-12-14 19:32:14 +0100

    feat: bring positions table

---
+++ src/routes/LPPortfolio/ConnectedPositionTable/PositionTable/components/PositionTableRow/components/Notional/Notional.tsx
@@ -0,0 +1,32 @@
+import isUndefined from 'lodash.isundefined';
+import React from 'react';
+
+import { Button } from '../../../../../../../../components/atomic/Button/Button';
+import { Typography } from '../../../../../../../../components/atomic/Typography/Typography';
+
+export type NotionalProps = {
+  notional?: string;
+  onEdit?: () => void;
+  token: string;
+};
+
+export const Notional: React.FunctionComponent<NotionalProps> = ({ notional, onEdit, token }) => {
+  return (
+    <>
+      <Typography label="Notional" sx={{ fontSize: 18 }} variant="body2">
+        {isUndefined(notional) ? 'Loading...' : `${notional} ${token}`}
+      </Typography>
+
+      {onEdit && (
+        <Button
+          size="small"
+          sx={{ width: '100%', display: 'flex' }}
+          variant="red2"
+          onClick={onEdit}
+        >
+          Edit
+        </Button>
+      )}
+    </>
+  );
+};
---
+++ src/routes/LPPortfolio/ConnectedPositionTable/PositionTable/types.ts
@@ -0,0 +1,8 @@
+export type PositionTableFields =
+  | 'pool'
+  | 'notional'
+  | 'margin'
+  | 'maturity'
+  | 'fixedApr'
+  | 'rateRange'
+  | 'accruedRates';
