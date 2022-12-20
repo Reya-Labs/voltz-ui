b244d15654075fafd35095bf0f6322291d51e3c7
ssbarbee <ssbarbee@users.noreply.github.com>
2022-12-20 11:56:11 +0100

    fix: allow for input of decimals in input form

--- src/routes/LPOptimisers/VaultFormRoute/Form/DepositAmountInput/DepositAmountInput.tsx
+++ src/routes/LPOptimisers/VaultFormRoute/Form/DepositAmountInput/DepositAmountInput.tsx
@@ -1,42 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { IconLabel } from '../../../../../components/composite/IconLabel/IconLabel';
 import { InputTokenLabel } from '../../../../../components/composite/InputTokenLabel/InputTokenLabel';
+import { doNothing } from '../../../../../utilities/doNothing';
+import { toUSFormat } from '../../../../../utilities/number';
 import { MaskedIntegerFieldStyled } from './DepositAmountInput.styled';
 
 type Props = {
   subtext: string;
   token: string;
-  value: string;
+  value: number;
   disabled: boolean;
-  onChange?: (newValue: string | undefined) => void;
+  onChange?: (newValue: number) => void;
 };
 export const DepositAmountInput: React.FunctionComponent<Props> = ({
   subtext,
   token,
-  onChange,
+  onChange = doNothing,
   disabled,
   value,
-}) => (
-  <MaskedIntegerFieldStyled
-    allowNegativeValue={false}
-    defaultValue={0}
-    disabled={disabled}
-    label={
-      <IconLabel
-        icon="information-circle"
-        info={
-          'Choose the amount you wish to deposit into the strategy. Remember you won’t be able to withdraw until the pool matures. '
+}) => {
+  const [inputValue, setInputValue] = useState(toUSFormat(value.toString()));
+  const handleOnBlur = (nextValue: number) => {
+    onChange(nextValue);
+    setInputValue(toUSFormat(nextValue.toString()));
+  };
+
+  return (
+    <MaskedIntegerFieldStyled
+      allowNegativeValue={false}
+      defaultValue={0}
+      disabled={disabled}
+      label={
+        <IconLabel
+          icon="information-circle"
+          info={
+            'Choose the amount you wish to deposit into the strategy. Remember you won’t be able to withdraw until the pool matures. '
+          }
+          label={'AMOUNT'}
+        />
+      }
+      subtext={subtext}
+      subtextSize={12}
+      suffix={<InputTokenLabel tokenName={token} />}
+      suffixPadding={90}
+      value={inputValue}
+      allowDecimals
+      onBlur={() => {
+        const valueParsed = parseFloat(inputValue || '0');
+        if (isNaN(valueParsed)) {
+          handleOnBlur(0);
+          return;
         }
-        label={'AMOUNT'}
-      />
-    }
-    subtext={subtext}
-    subtextSize={12}
-    suffix={<InputTokenLabel tokenName={token} />}
-    suffixPadding={90}
-    value={value}
-    allowDecimals
-    onChange={onChange}
-  />
-);
+        handleOnBlur(valueParsed);
+      }}
+      onChange={setInputValue}
+    />
+  );
+};
