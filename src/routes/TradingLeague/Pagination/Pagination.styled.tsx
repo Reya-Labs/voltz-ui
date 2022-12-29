e4b83ec93a28ba45fae670c7308d1e43dfa10a4e
ssbarbee <ssbarbee@users.noreply.github.com>
2022-12-30 00:18:20 +0100

    chore: remove deprecated LPPortfolio

---
+++ src/routes/TradingLeague/Pagination/Pagination.styled.tsx
@@ -0,0 +1,47 @@
+import ChevronLeft from '@mui/icons-material/ChevronLeft';
+import ChevronRight from '@mui/icons-material/ChevronRight';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+
+import { colors } from '../../../theme';
+
+export const PaginationBox = styled(Box)`
+  display: flex;
+  justify-content: center;
+  margin-top: ${({ theme }) => theme.spacing(4)};
+`;
+
+export const ActionButton = styled(Button)`
+  font-size: 12px;
+  line-height: 12px;
+  color: ${colors.lavenderWeb.base};
+  padding: ${({ theme }) => theme.spacing(2)};
+`;
+
+export const BarBox = styled(Box)`
+  width: 96px;
+  background: ${colors.lavenderWeb.darken030};
+  margin: ${({ theme }) => theme.spacing(2)};
+`;
+
+export const AnimatedBarBox = styled(Box, {
+  shouldForwardProp: (prop) => prop !== 'width',
+})<{ width: number }>`
+  width: ${({ width }) => width}%;
+  background: ${colors.lavenderWeb.base};
+  height: 100%;
+  transition: width 500ms ease-in;
+`;
+
+export const ChevronLeftIcon = styled(ChevronLeft)`
+  width: 14px;
+  height: 14px;
+  color: ${colors.lavenderWeb.base};
+`;
+
+export const ChevronRightIcon = styled(ChevronRight)`
+  width: 14px;
+  height: 14px;
+  color: ${colors.lavenderWeb.base};
+`;
