bf992d3586f79ca0ed4241f98629db48fe212379
ssbarbee <ssbarbee@users.noreply.github.com>
2022-12-29 14:44:53 +0100

    fix: hide optimisors section when active transaction

--- src/store/features/transactions/selectors/transactions/index.ts
+++ src/store/features/transactions/selectors/transactions/index.ts
@@ -1,2 +1,3 @@
 export { default as transactionSelector } from './transactionSelector';
+export { default as transactionsSelector } from './transactionsSelector';
 export { default as unresolvedTransactionsSelector } from './unresolvedTransactionsSelector';
