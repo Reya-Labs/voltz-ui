9ff148029dc9558434cddd39c51d092d189399cc
ssbarbee <ssbarbee@users.noreply.github.com>
2023-01-09 09:13:16 +0100

    test: add Tick.test

--- src/components/atomic/Tick/Tick.tsx
+++ src/components/atomic/Tick/Tick.tsx
@@ -10,13 +10,18 @@ export const Tick: React.FunctionComponent<TickProps> = React.memo(({ onAnimatio
   <IconAnimated data-testid="Tick">
     <TickContainer>
       <TickPart1
+        data-testid="Tick-TickPart1"
         viewBox="0 0 25 52"
         xmlns="http://www.w3.org/2000/svg"
         onAnimationEnd={onAnimationEnd}
       >
         <polygon className="" points="1,41 0,48 25,52 25,45" />
       </TickPart1>
-      <TickPart2 viewBox="0 0 25 52" xmlns="http://www.w3.org/2000/svg">
+      <TickPart2
+        data-testid="Tick-TickPart2"
+        viewBox="0 0 25 52"
+        xmlns="http://www.w3.org/2000/svg"
+      >
         <polygon className="" points="18,45 25,47 25,0 18,0" />
       </TickPart2>
     </TickContainer>
