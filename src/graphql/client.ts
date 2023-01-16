be2edb8a2190443c635cfea15a9b97679e4cbebd
tudor47 <tudorcostin7@gmail.com>
2023-01-17 00:32:06 +0200

    feat: removed queries

--- src/graphql/client.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: process.env.REACT_APP_SUBGRAPH_URL,
-  cache: new InMemoryCache(),
-});
-
-export default client;
--- src/graphql/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as client } from './client';
-export * from './generated';
-export { default as VoltzGraphProvider } from './VoltzGraphProvider';
