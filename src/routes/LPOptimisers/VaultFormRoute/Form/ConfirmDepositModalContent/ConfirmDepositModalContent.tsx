f61b8085d811affe0228ee0e5f22013ccc48de64
ssbarbee <ssbarbee@users.noreply.github.com>
2023-01-24 15:18:48 +0100

    test: add ConfirmDepositModalContent.test.tsx

--- src/routes/LPOptimisers/VaultFormRoute/Form/ConfirmDepositModalContent/ConfirmDepositModalContent.tsx
+++ src/routes/LPOptimisers/VaultFormRoute/Form/ConfirmDepositModalContent/ConfirmDepositModalContent.tsx
@@ -48,9 +48,11 @@ export const ConfirmDepositModalContent: React.FunctionComponent<Props> = ({
   depositFeeUnderlying,
   token,
 }) => (
-  <ContentBox>
-    <TitleTypography>Deposit</TitleTypography>
-    <DescriptionTypography>
+  <ContentBox data-testid="ConfirmDepositModalContent-ContentBox">
+    <TitleTypography data-testid="ConfirmDepositModalContent-TitleTypography">
+      Deposit
+    </TitleTypography>
+    <DescriptionTypography data-testid="ConfirmDepositModalContent-DescriptionTypography">
       When depositing funds in the Optimiser, users pay a contribution to the batch budget fund.
       Batches are done to reduce the gas cost of depositing in pools by splitting the costs between
       those depositing funds.
@@ -60,15 +62,17 @@ export const ConfirmDepositModalContent: React.FunctionComponent<Props> = ({
       <DepositBudgetTextTypography>BATCH FEE&nbsp;</DepositBudgetTextTypography>
       {depositFeeUSD === -1 || depositFeeUnderlying === -1 ? (
         <DepositBudgetValueBox>
-          <DepositBudgetTextTypography>---</DepositBudgetTextTypography>
+          <DepositBudgetTextTypography data-testid="ConfirmDepositModalContent-DepositFeeLoading">
+            ---
+          </DepositBudgetTextTypography>
         </DepositBudgetValueBox>
       ) : (
         <DepositBudgetValueBox>
-          <DepositBudgetUnderlyingTypography>
+          <DepositBudgetUnderlyingTypography data-testid="ConfirmDepositModalContent-DepositBudgetUnderlyingTypography">
             {formatCurrency(depositFeeUnderlying)}&nbsp;
             {token.toUpperCase()}
           </DepositBudgetUnderlyingTypography>
-          <DepositBudgetTextTypography>
+          <DepositBudgetTextTypography data-testid="ConfirmDepositModalContent-DepositBudgetTextTypography">
             <DepositBudgetUSDCurrencyTypography>$</DepositBudgetUSDCurrencyTypography>
             {formatCurrency(depositFeeUSD)} USD
           </DepositBudgetTextTypography>
