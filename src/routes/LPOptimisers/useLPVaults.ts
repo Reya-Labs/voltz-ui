db4408a30c76e602c5f6fff1a7385636698121f9
tudor47 <tudorcostin7@gmail.com>
2023-01-31 13:19:50 +0200

    feat: added selector for loaded state

--- src/routes/LPOptimisers/useLPVaults.ts
+++ src/routes/LPOptimisers/useLPVaults.ts
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import {
   initialiseOptimisersThunk,
   selectOptimisers,
+  selectOptimisersLoadedState,
 } from '../../app/features/stateless-optimisers';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { useWallet } from '../../hooks/useWallet';
@@ -10,7 +11,7 @@ import { useWallet } from '../../hooks/useWallet';
 export const useLPVaults = () => {
   const { signer } = useWallet();
 
-  const vaultsLoaded = useAppSelector((state) => state.statelessOptimisers.optimisersLoadedState);
+  const vaultsLoaded = useAppSelector(selectOptimisersLoadedState);
   const lpVaults = useAppSelector(selectOptimisers);
   const dispatch = useAppDispatch();
 
