eba72bd5c16014c8a0795e2c23baaa3d10b38b14
tudor47 <tudorcostin7@gmail.com>
2023-01-31 16:15:43 +0200

    feat: some fixes

--- src/app/features/stateless-optimisers/reducer.ts
+++ src/app/features/stateless-optimisers/reducer.ts
@@ -1,22 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-import { depositOptimisersThunk, initialiseOptimisersThunk } from './thunks';
+import { initialiseOptimisersThunk } from './thunks';
 import { OptimiserInfo } from './types';
 
 type SliceState = {
   optimisersLoadedState: 'idle' | 'pending' | 'succeeded' | 'failed';
   optimisers: OptimiserInfo[];
-
-  depositLoadedState: 'idle' | 'pending' | 'succeeded' | 'failed';
-  depositError: string;
 };
 
 const initialState: SliceState = {
   optimisersLoadedState: 'idle',
   optimisers: [],
-
-  depositLoadedState: 'idle',
-  depositError: '',
 };
 
 export const slice = createSlice({
@@ -36,24 +30,6 @@ export const slice = createSlice({
         state.optimisersLoadedState = 'succeeded';
         state.optimisers = action.payload as OptimiserInfo[];
       })
-      .addCase(depositOptimisersThunk.pending, (state) => {
-        state.depositLoadedState = 'pending';
-      })
-      .addCase(depositOptimisersThunk.rejected, (state, action) => {
-        state.depositLoadedState = 'failed';
-        state.depositError = `Deposit failed. ${action.error.message ?? ''}`;
-      })
-      .addCase(depositOptimisersThunk.fulfilled, (state, action) => {
-        state.depositLoadedState = 'succeeded';
-        const optimiserId = action.meta.arg.optimiserId;
-
-        const optimiserIndex = state.optimisers.findIndex(
-          (optimiser) => optimiser.optimiserId === optimiserId,
-        );
-        if (optimiserIndex >= 0) {
-          state.optimisers[optimiserIndex] = action.payload as OptimiserInfo;
-        }
-      });
   },
 });
 
