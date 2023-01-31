8584ee129395fff176ac73fc3ca7ff3e2c830a4b
tudor47 <tudorcostin7@gmail.com>
2023-01-31 13:19:50 +0200

    feat: added selector for loaded state

--- src/app/features/stateless-optimisers/selectors.ts
+++ src/app/features/stateless-optimisers/selectors.ts
@@ -4,3 +4,7 @@ import { OptimiserInfo } from './types';
 export const selectOptimisers = (state: RootState): OptimiserInfo[] => {
   return state.statelessOptimisers.optimisers;
 };
+
+export const selectOptimisersLoadedState = (state: RootState) => {
+  return state.statelessOptimisers.optimisersLoadedState;
+};
