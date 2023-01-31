f1871c524c54c789a96c72527125cde21dc7e7c0
tudor47 <tudorcostin7@gmail.com>
2023-01-31 13:31:13 +0200

    feat: sdk bump

--- src/routes/LPOptimisers/Helpers/helpers.test.ts
+++ src/routes/LPOptimisers/Helpers/helpers.test.ts
@@ -1,30 +1,47 @@
-import {
-    getSpareWeights
-} from './getSpareWeights';
+import { getSpareWeights } from './getSpareWeights';
 
 describe('getSpareWeights', () => {
-    it('non-zero weights with 1 element', () => {
-        const spareWeights = getSpareWeights([{ vaultId: 'A' }], [{ distribution: 100 }]);
-        expect(spareWeights).toEqual([['A', 100]]);
-    });
+  it('non-zero weights with 1 element', () => {
+    const spareWeights = getSpareWeights([{ vaultId: 'A' }], [{ distribution: 100 }]);
+    expect(spareWeights).toEqual([['A', 100]]);
+  });
 
-    it('non-zero weights with more elements', () => {
-        const spareWeights = getSpareWeights([{ vaultId: 'A' }, { vaultId: 'C' }], [{ distribution: 50 }, { distribution: 50 }]);
-        expect(spareWeights).toEqual([['A', 50], ['C', 50]]);
-    });
+  it('non-zero weights with more elements', () => {
+    const spareWeights = getSpareWeights(
+      [{ vaultId: 'A' }, { vaultId: 'C' }],
+      [{ distribution: 50 }, { distribution: 50 }],
+    );
+    expect(spareWeights).toEqual([
+      ['A', 50],
+      ['C', 50],
+    ]);
+  });
 
-    it('some zero weights with more elements', () => {
-        const spareWeights = getSpareWeights([{ vaultId: 'A' }, { vaultId: 'C' }, { vaultId: 'D' }, { vaultId: 'E' }], [{ distribution: 50 }, { distribution: 30 }, { distribution: 0 }, { distribution: 20 }]);
-        expect(spareWeights).toEqual([['A', 50], ['C', 30], ['E', 20]]);
-    });
+  it('some zero weights with more elements', () => {
+    const spareWeights = getSpareWeights(
+      [{ vaultId: 'A' }, { vaultId: 'C' }, { vaultId: 'D' }, { vaultId: 'E' }],
+      [{ distribution: 50 }, { distribution: 30 }, { distribution: 0 }, { distribution: 20 }],
+    );
+    expect(spareWeights).toEqual([
+      ['A', 50],
+      ['C', 30],
+      ['E', 20],
+    ]);
+  });
 
-    it('length of vaults greater than length of weights', () => {
-        const spareWeights = getSpareWeights([{ vaultId: 'A' }, { vaultId: 'C' }], [{ distribution: 50 }]);
-        expect(spareWeights).toEqual([]);
-    });
+  it('length of vaults greater than length of weights', () => {
+    const spareWeights = getSpareWeights(
+      [{ vaultId: 'A' }, { vaultId: 'C' }],
+      [{ distribution: 50 }],
+    );
+    expect(spareWeights).toEqual([]);
+  });
 
-    it('length of vaults smaller than length of weights', () => {
-        const spareWeights = getSpareWeights([{ vaultId: 'A' }], [{ distribution: 50 }, { distribution: 50 }]);
-        expect(spareWeights).toEqual([]);
-    });
+  it('length of vaults smaller than length of weights', () => {
+    const spareWeights = getSpareWeights(
+      [{ vaultId: 'A' }],
+      [{ distribution: 50 }, { distribution: 50 }],
+    );
+    expect(spareWeights).toEqual([]);
+  });
 });
