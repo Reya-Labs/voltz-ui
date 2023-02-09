08399110734393f2a9fb9fbc35395e2acef4ff02
ssbarbee <ssbarbee@users.noreply.github.com>
2023-02-10 00:08:57 +0100

    feat: add listener middleware for rearming sdk

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,7 +4,7 @@ import createSagaMiddleware from 'redux-saga';
 
 import { aMMsReducer } from './features/aMMs';
 import { lpOptimisersReducer } from './features/lp-optimisers';
-import { networkReducer } from './features/network';
+import { networkListenerMiddleware, networkReducer } from './features/network';
 import { statelessOptimisersReducer } from './features/stateless-optimisers';
 import { saga, transactionsReducer } from './features/transactions';
 
@@ -22,7 +22,12 @@ export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
   const store = configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(logger).concat(sagaMiddleware),
+      getDefaultMiddleware()
+        .concat(logger)
+        .concat(sagaMiddleware)
+        // NOTE: Since this can receive actions with functions inside,
+        // it should go before the serializability check middleware
+        .prepend(networkListenerMiddleware.middleware),
     preloadedState,
   });
   sagaMiddleware.run(saga);
