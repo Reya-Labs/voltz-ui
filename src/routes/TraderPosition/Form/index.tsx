d58ef6fa7235976882109eec3eba36bae56b204a
ssbarbee <ssbarbee@users.noreply.github.com>
2023-02-21 23:42:38 +0100

    feat: add swap rates form initial ui

--- src/routes/TraderPosition/Form/index.tsx
+++ src/routes/TraderPosition/Form/index.tsx
@@ -1,12 +1,22 @@
 import { Typography } from 'brokoli-ui';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Ellipsis } from '../../../components/atomic/Ellipsis/Ellipsis';
+import { FormBox } from './Form.styled';
+import { NotionalSwap } from './NotionalSwap';
 
 export const Form: React.FunctionComponent = () => {
+  const [mode, setMode] = useState<'fixed' | 'variable'>('fixed');
   return (
-    <Typography typographyToken="primaryBodyExtraLargeRegular">
-      Engineers at work 👨🛠📋 <Ellipsis />
-    </Typography>
+    <FormBox>
+      <Typography colorToken="lavenderWeb" typographyToken="primaryHeader3Black">
+        SWAP Rates
+      </Typography>
+      <NotionalSwap fixedRate={5.49} mode={mode} variableRate={2.49} onSwap={setMode} />
+
+      <Typography typographyToken="primaryBodyExtraLargeRegular">
+        Engineers at work 👨🛠📋 <Ellipsis />
+      </Typography>
+    </FormBox>
   );
 };
