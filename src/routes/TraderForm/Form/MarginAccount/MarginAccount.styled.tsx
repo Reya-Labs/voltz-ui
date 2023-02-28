8a587d5a5d003143aed9f6f7bc2cf29ccec81831
ssbarbee <ssbarbee@users.noreply.github.com>
2023-02-28 11:48:45 +0100

    feat: add MarginAccount component

---
+++ src/routes/TraderForm/Form/MarginAccount/MarginAccount.styled.tsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+import { colors } from 'brokoli-ui';
+
+export const MarginAccountBox = styled('div')`
+  box-sizing: border-box;
+
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 8px 40px;
+  gap: 16px;
+
+  width: 100%;
+  background: ${colors.lavenderWeb8};
+  border: 1px solid ${colors.lavenderWeb8};
+`;
+
+export const AccountBox = styled('div')``;
+export const BalanceBox = styled('div')``;
