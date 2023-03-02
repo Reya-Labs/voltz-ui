8b9cb680bca74b21ffedbad43d1614bb599b6acf
Alexandru Niculae <niculae.alexandru1@gmail.com>
2023-03-02 17:11:53 +0000

    feat: address comments

--- src/routes/TraderForm/Form/MarginAccount/index.tsx
+++ src/routes/TraderForm/Form/MarginAccount/index.tsx
@@ -1,15 +1,24 @@
 import { LabelTokenTypography } from 'brokoli-ui';
 import React from 'react';
 
+import { selectSwapFormAMM, selectWalletBalanceInfo } from '../../../../app/features/swap-form';
+import { useAppSelector } from '../../../../app/hooks';
+import { formatTimestamp } from '../../../../utilities/date';
+import { compactFormat } from '../../../../utilities/number';
 import { AccountBox, BalanceBox, MarginAccountBox } from './MarginAccount.styled';
 
 type MarginAccountProps = {};
 
 export const MarginAccount: React.FunctionComponent<MarginAccountProps> = () => {
-  const balanceValue = '12k';
-  const token = ' USDC';
-  // The name of the pool is “{pool_token} {maturity}” (e.g. aUSDT_bor Dec22, aETH_len Mar23, stETH Jan23);
-  const accountName = 'aUSDT_bor Dec22';
+  const aMM = useAppSelector(selectSwapFormAMM);
+  const walletBalance = useAppSelector(selectWalletBalanceInfo);
+
+  const balanceValue =
+    walletBalance.status === 'success' ? compactFormat(walletBalance.value) : '--';
+  const token = ` ${aMM ? aMM.underlyingToken.name.toUpperCase() : ''}`;
+
+  const accountName = aMM ? `${aMM.protocol} ${formatTimestamp(aMM.termEndTimestampInMS)}` : '';
+
   return (
     <MarginAccountBox>
       <AccountBox>
