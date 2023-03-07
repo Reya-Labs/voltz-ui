15d87cd1cc37fa980a5ed5270640206b3819828d
Alexandru Niculae <niculae.alexandru1@gmail.com>
2023-03-07 17:04:14 +0000

    fix: transaction details

--- src/routes/TraderForm/Form/TransactionDetails/index.tsx
+++ src/routes/TraderForm/Form/TransactionDetails/index.tsx
@@ -1,7 +1,11 @@
 import { TokenTypography, Typography } from 'brokoli-ui';
 import React from 'react';
 
-import { selectInfoPostSwap, selectSwapFormAMM } from '../../../../app/features/swap-form';
+import {
+  selectInfoPostSwap,
+  selectSubmitButtonInfo,
+  selectSwapFormAMM,
+} from '../../../../app/features/swap-form';
 import { useAppSelector } from '../../../../app/hooks';
 import { formatNumber } from '../../../../utilities/number';
 import { ReactComponent as GasIcon } from './gas-icon.svg';
@@ -12,31 +16,66 @@ type TransactionDetailsProps = {};
 export const TransactionDetails: React.FunctionComponent<TransactionDetailsProps> = () => {
   const infoPostSwap = useAppSelector(selectInfoPostSwap);
   const aMM = useAppSelector(selectSwapFormAMM);
+  const submitButtonInfo = useAppSelector(selectSubmitButtonInfo);
 
   if (!aMM) {
     return null;
   }
 
+  const hideFees = submitButtonInfo.state === 'margin-update';
+  const hideSlippage = submitButtonInfo.state === 'margin-update';
+  const hideGasFees = false;
+
   return (
     <React.Fragment>
-      <TransactionDetailBox>
-        <IconTextWrapper>
-          <GasIcon />
+      {hideFees ? null : (
+        <TransactionDetailBox>
           <Typography colorToken="lavenderWeb2" typographyToken="primaryBodySmallRegular">
-            Gas Fees
+            Fees
           </Typography>
-        </IconTextWrapper>
-        <TokenTypography
-          colorToken="lavenderWeb"
-          token=" ETH"
-          typographyToken="secondaryBodySmallRegular"
-          value={
-            infoPostSwap.status === 'success'
-              ? formatNumber(infoPostSwap.value.gasFeeETH, 2, 4)
-              : '--'
-          }
-        ></TokenTypography>
-      </TransactionDetailBox>
+          <TokenTypography
+            colorToken="lavenderWeb"
+            token={` ${aMM.underlyingToken.name.toUpperCase()}`}
+            typographyToken="secondaryBodySmallRegular"
+            value={infoPostSwap.status === 'success' ? formatNumber(infoPostSwap.value.fee) : '--'}
+          ></TokenTypography>
+        </TransactionDetailBox>
+      )}
+      {hideSlippage ? null : (
+        <TransactionDetailBox>
+          <Typography colorToken="lavenderWeb2" typographyToken="primaryBodySmallRegular">
+            Estimated Slippage
+          </Typography>
+          <TokenTypography
+            colorToken="lavenderWeb"
+            token="%"
+            typographyToken="secondaryBodySmallRegular"
+            value={
+              infoPostSwap.status === 'success' ? formatNumber(infoPostSwap.value.slippage) : '--'
+            }
+          ></TokenTypography>
+        </TransactionDetailBox>
+      )}
+      {hideGasFees ? null : (
+        <TransactionDetailBox>
+          <IconTextWrapper>
+            <GasIcon />
+            <Typography colorToken="lavenderWeb2" typographyToken="primaryBodySmallRegular">
+              Gas Fees
+            </Typography>
+          </IconTextWrapper>
+          <TokenTypography
+            colorToken="lavenderWeb"
+            token=" ETH"
+            typographyToken="secondaryBodySmallRegular"
+            value={
+              infoPostSwap.status === 'success'
+                ? formatNumber(infoPostSwap.value.gasFeeETH, 2, 4)
+                : '--'
+            }
+          ></TokenTypography>
+        </TransactionDetailBox>
+      )}
     </React.Fragment>
   );
 };
