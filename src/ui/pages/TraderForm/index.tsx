8ceebd32fdf541f47c2f447d52f4cd25cfb56393
ssbarbee <ssbarbee@users.noreply.github.com>
2023-03-16 15:21:45 +0100

    feat: add VoltzPage component

--- src/ui/pages/TraderForm/index.tsx
+++ src/ui/pages/TraderForm/index.tsx
@@ -1,25 +1,23 @@
-import { Page as BrokoliPage, RainbowLoader } from 'brokoli-ui';
+import { RainbowLoader } from 'brokoli-ui';
 import * as React from 'react';
 import { useParams } from 'react-router-dom';
 
 import { resetStateAction } from '../../../app/features/swap-form';
 import { useAppDispatch } from '../../../app/hooks';
 import { useSwapFormAMM } from '../../../hooks/useSwapFormAMM';
-import { NoVaultFound } from '../../../routes/LPOptimisers/VaultFormRoute/NoVaultFound/NoVaultFound';
+import { VoltzPage } from '../../components/VoltzPage';
 import { Form } from './Form';
-import { LeftPanel } from './LeftPanel';
 import { Main } from './Main';
 import { NoAMMFound } from './NoAMMFound/NoAMMFound';
-import { TopPanel } from './TopPanel';
-import {
-  LoadingBox,
-  MainAndFormSectionBox,
-  MainSectionBox,
-  PageSectionBox,
-  RainbowLoadingBox,
-  RightPageSectionBox,
-  RightSectionBox,
-} from './TraderForm.styled';
+import { LoadingBox, RainbowLoadingBox } from './TraderForm.styled';
+
+const LoadingPage: React.FunctionComponent = React.memo(() => (
+  <LoadingBox>
+    <RainbowLoadingBox>
+      <RainbowLoader height={2} text="Fetching best rates..." />
+    </RainbowLoadingBox>
+  </LoadingBox>
+));
 
 export const TraderFormPage: React.FunctionComponent = () => {
   const dispatch = useAppDispatch();
@@ -33,42 +31,14 @@ export const TraderFormPage: React.FunctionComponent = () => {
     };
   }, []);
 
-  let pageContent = (
-    <MainAndFormSectionBox data-testid="BrokoliPage-MainAndFormSectionBox">
-      <MainSectionBox data-testid="BrokoliPage-MainSectionBox">
-        <Main />
-      </MainSectionBox>
-      <RightSectionBox data-testid="BrokoliPage-RightSectionBox">
-        <Form />
-      </RightSectionBox>
-    </MainAndFormSectionBox>
-  );
-
-  if (noAMMFound || error) {
-    pageContent = <NoAMMFound />;
-  } else if (loading) {
-    pageContent = (
-      <LoadingBox>
-        <RainbowLoadingBox>
-          <RainbowLoader height={2} text="Fetching best rates..." />
-        </RainbowLoadingBox>
-      </LoadingBox>
-    );
-  }
-
-  if (form !== 'swap') {
-    pageContent = <NoVaultFound />;
-  }
+  const pageNotFound = form !== 'swap' || noAMMFound || error;
 
   return (
-    <BrokoliPage data-testid="BrokoliPage">
-      <PageSectionBox data-testid="BrokoliPage-PageSectionBox">
-        <LeftPanel />
-        <RightPageSectionBox data-testid="BrokoliPage-RightPageSectionBox">
-          <TopPanel />
-          {pageContent}
-        </RightPageSectionBox>
-      </PageSectionBox>
-    </BrokoliPage>
+    <VoltzPage
+      MainSlotComponent={Main}
+      NotFoundSlotComponent={pageNotFound ? NoAMMFound : undefined}
+      PageLoadingSlotComponent={loading ? LoadingPage : undefined}
+      RightSlotComponent={Form}
+    />
   );
 };
