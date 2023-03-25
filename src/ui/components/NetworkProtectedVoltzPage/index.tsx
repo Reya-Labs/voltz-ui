97d2d5f130207eb7ff40800f9440d267da696548
ssbarbee <ssbarbee@users.noreply.github.com>
2023-03-25 15:17:43 +0100

    chore: import

--- src/ui/components/NetworkProtectedVoltzPage/index.tsx
+++ src/ui/components/NetworkProtectedVoltzPage/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { selectIsSupportedChain } from '../../../app/features/network';
 import { useAppSelector } from '../../../app/hooks';
-import { ConnectSupportedNetwork } from '../ConnectSupportedNetwork/ConnectSupportedNetwork';
+import { ConnectSupportedNetwork } from '../ConnectSupportedNetwork';
 import { NotFoundPageContent } from '../NotFoundPageContent';
 import { VoltzPage } from '../VoltzPage';
 
@@ -18,7 +18,6 @@ export const NetworkProtectedVoltzPage: React.FunctionComponent<{
       <VoltzPage
         mainSlot={
           <ConnectSupportedNetwork
-            connectSupportedNetworkText="CONNECT WITH A SUPPORTED NETWORK"
             heading="⚠️ RESTRICTED"
             subheading="Your wallet needs to be connected to a supported network."
           />
