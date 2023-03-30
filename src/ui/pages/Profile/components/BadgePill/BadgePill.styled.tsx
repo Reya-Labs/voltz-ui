52c695d4aabfc4f27dba29359f0af8c1ede2f0aa
ssbarbee <ssbarbee@users.noreply.github.com>
2023-03-30 12:13:47 +0200

    fix: use easter egg from brokoli

--- src/ui/pages/Profile/components/BadgePill/BadgePill.styled.tsx
+++ src/ui/pages/Profile/components/BadgePill/BadgePill.styled.tsx
@@ -1,12 +1,8 @@
 import styled from '@emotion/styled';
-import { Pill as PillComponent, Skeleton as SkeletonComponent } from 'brokoli-ui';
+import { Skeleton as SkeletonComponent } from 'brokoli-ui';
 
 export const Skeleton = styled(SkeletonComponent)`
   font-size: 14px;
   width: 60px;
   border-radius: 4px;
-`;
-
-export const Pill = styled(PillComponent)`
-  padding: 2px 8px !important;
 `;
