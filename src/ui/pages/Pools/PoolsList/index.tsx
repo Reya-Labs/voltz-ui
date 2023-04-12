03878df90dcf1ba7f2ca128ac677b3f18ec029b0
ssbarbee <ssbarbee@users.noreply.github.com>
2023-04-12 15:57:33 +0200

    feat: add no-pools-found

--- src/ui/pages/Pools/PoolsList/index.tsx
+++ src/ui/pages/Pools/PoolsList/index.tsx
@@ -1,14 +1,21 @@
+import { Typography } from 'brokoli-ui';
 import React from 'react';
 
 import { selectPools, selectPoolsLoading } from '../../../../app/features/aMMs';
 import { useAppSelector } from '../../../../app/hooks';
 import { PoolEntry } from './PoolEntry';
 import { PoolsHeader } from './PoolsHeader';
-import { PoolEntrySkeleton, PoolsHeaderAndListBox, PoolsListBox } from './PoolsList.styled';
+import {
+  NoPoolsFoundBox,
+  PoolEntrySkeleton,
+  PoolsHeaderAndListBox,
+  PoolsListBox,
+} from './PoolsList.styled';
 
 export const PoolsList: React.FunctionComponent = () => {
   const loading = useAppSelector(selectPoolsLoading);
   const pools = useAppSelector(selectPools);
+  const noPoolsMatchCriteria = !loading && pools.length === 0;
 
   return (
     <PoolsHeaderAndListBox>
@@ -24,24 +31,31 @@ export const PoolsList: React.FunctionComponent = () => {
               />
             ))
           : null}
-        {!loading
-          ? pools.map((pool, index) => (
-              <PoolEntry
-                key={pool.id}
-                aMMMaturity={pool.aMMMaturity}
-                backgroundColorToken={index % 2 !== 0 ? 'liberty7' : 'lavenderWeb8'}
-                fixedRateFormatted={pool.fixedRateFormatted}
-                isAaveV3={pool.isAaveV3}
-                isBorrowing={pool.isBorrowing}
-                market={pool.market}
-                routeAmmId={pool.routeAmmId}
-                routePoolId={pool.routePoolId}
-                token={pool.token}
-                variableRate24hDelta={pool.variableRate24hDelta}
-                variableRateFormatted={pool.variableRateFormatted}
-              />
-            ))
-          : null}
+        {!noPoolsMatchCriteria ? (
+          pools.map((pool, index) => (
+            <PoolEntry
+              key={pool.id}
+              aMMMaturity={pool.aMMMaturity}
+              backgroundColorToken={index % 2 !== 0 ? 'liberty7' : 'lavenderWeb8'}
+              fixedRateFormatted={pool.fixedRateFormatted}
+              isAaveV3={pool.isAaveV3}
+              isBorrowing={pool.isBorrowing}
+              market={pool.market}
+              routeAmmId={pool.routeAmmId}
+              routePoolId={pool.routePoolId}
+              token={pool.token}
+              variableRate24hDelta={pool.variableRate24hDelta}
+              variableRateFormatted={pool.variableRateFormatted}
+            />
+          ))
+        ) : (
+          <NoPoolsFoundBox>
+            <img alt="Gimme" src="/images/no-pools-found.png" />
+            <Typography colorToken="lavenderWeb" typographyToken="primaryBodyLargeBold">
+              No Pools matching the filter criteria
+            </Typography>
+          </NoPoolsFoundBox>
+        )}
       </PoolsListBox>
     </PoolsHeaderAndListBox>
   );
