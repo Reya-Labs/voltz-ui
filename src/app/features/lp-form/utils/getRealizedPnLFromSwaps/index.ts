11b2d997dfa684905adb243bbfaaa874ad967c06
ssbarbee <ssbarbee@users.noreply.github.com>
2023-04-18 23:09:07 +0200

    refactor: remove comment

--- src/app/features/lp-form/utils/getRealizedPnLFromSwaps/index.ts
+++ src/app/features/lp-form/utils/getRealizedPnLFromSwaps/index.ts
@@ -1,14 +1,12 @@
 import { Draft } from '@reduxjs/toolkit';
+import { Position } from '@voltz-protocol/v1-sdk';
 
 import { SliceState } from '../../reducer';
 import { hasExistingPosition } from '../hasExistingPosition';
 
 export const getRealizedPnLFromSwaps = (state: Draft<SliceState>) => {
   if (hasExistingPosition(state)) {
-    // feels redundunt
-    if (state.selectedPosition) {
-      return state.selectedPosition.realizedPnLFromSwaps;
-    }
+    return (state.selectedPosition as Position).realizedPnLFromSwaps;
   }
   return null;
 };
