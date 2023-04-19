8ff330edf0f8cc8a3f863844186d1b53b807239a
ssbarbee <ssbarbee@users.noreply.github.com>
2023-04-19 12:27:53 +0200

    test: add tests for getSelectedPositionNotional

--- src/app/features/lp-form/utils/getSelectedPositionNotional/index.ts
+++ src/app/features/lp-form/utils/getSelectedPositionNotional/index.ts
@@ -3,11 +3,5 @@ import { Draft } from '@reduxjs/toolkit';
 import { SliceState } from '../../reducer';
 
 export const getSelectedPositionNotional = (state: Draft<SliceState>) => {
-  let selectedPositionNotional = 0;
-
-  if (state.selectedPosition !== null) {
-    selectedPositionNotional += state.selectedPosition.notional;
-  }
-
-  return selectedPositionNotional;
+  return state.selectedPosition?.notional ?? 0;
 };
