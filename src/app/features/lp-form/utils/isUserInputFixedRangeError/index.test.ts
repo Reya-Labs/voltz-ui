33a6c532462b5a2da9502468e19a6a3af159d48a
ssbarbee <ssbarbee@users.noreply.github.com>
2023-04-19 12:29:41 +0200

    test: add tests for isUserInputFixedRangeError

---
+++ src/app/features/lp-form/utils/isUserInputFixedRangeError/index.test.ts
@@ -0,0 +1,16 @@
+import { isUserInputFixedRangeError } from './';
+
+describe('isUserInputFixedRangeError', () => {
+  it('should return false when userInput.fixedRange.error is null', () => {
+    const state = { userInput: { fixedRange: { error: null } } };
+    const result = isUserInputFixedRangeError(state as never);
+    expect(result).toEqual(false);
+  });
+
+  it('should return true when userInput.fixedRange.error is not null', () => {
+    const error = new Error('Invalid fixed range');
+    const state = { userInput: { fixedRange: { error } } };
+    const result = isUserInputFixedRangeError(state as never);
+    expect(result).toEqual(true);
+  });
+});
