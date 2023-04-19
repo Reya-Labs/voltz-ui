a0f4dd4d905e00a0c3227b0f504a0cc05d4f3229
ssbarbee <ssbarbee@users.noreply.github.com>
2023-04-19 13:55:59 +0200

    test: add tests for updateSelectedPosition

--- src/app/features/lp-form/utils/updateSelectedPosition/index.ts
+++ src/app/features/lp-form/utils/updateSelectedPosition/index.ts
@@ -3,25 +3,19 @@ import { Draft } from '@reduxjs/toolkit';
 import { SliceState } from '../../reducer';
 
 export const updateSelectedPosition = (state: Draft<SliceState>): void => {
-  if (
-    state.positions.status !== 'success' ||
-    state.positions.value === null ||
-    state.positions.value.length === 0
-  ) {
+  const positions = state.positions;
+
+  if (positions.status !== 'success' || !positions.value?.length) {
     return;
   }
 
-  const fixedLower = state.userInput.fixedRange.lower;
-  const fixedUpper = state.userInput.fixedRange.upper;
-  state.selectedPosition = state.positions.value[0];
+  const { lower: fixedLower, upper: fixedUpper } = state.userInput.fixedRange;
 
-  const filteredPosition = state.positions.value.find(
-    (i) => i.fixedRateLower.toNumber() === fixedLower && i.fixedRateUpper.toNumber() === fixedUpper,
+  const filteredPosition = positions.value.find(
+    (position) =>
+      position.fixedRateLower.toNumber() === fixedLower &&
+      position.fixedRateUpper.toNumber() === fixedUpper,
   );
 
-  if (filteredPosition === undefined) {
-    state.selectedPosition = null;
-  } else {
-    state.selectedPosition = filteredPosition;
-  }
+  state.selectedPosition = filteredPosition || null;
 };
