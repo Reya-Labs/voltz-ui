b782f47ce3b567483e10325a7b8100abd04ca11d
ssbarbee <ssbarbee@users.noreply.github.com>
2023-04-24 10:53:18 +0200

    test: add test for selectLpFormAMM

--- src/app/features/forms/lp-form/selectors.test.ts
+++ src/app/features/forms/lp-form/selectors.test.ts
@@ -1,4 +1,4 @@
-import { selectSubmitButtonInfo } from '../lp-form';
+import { selectLpFormAMM, selectSubmitButtonInfo } from '../lp-form';
 
 describe('lp-form.selectors', () => {
   describe('selectSubmitButtonInfo', () => {
@@ -23,6 +23,34 @@ describe('lp-form.selectors', () => {
           text: null,
           isError: false,
         },
+      });
+    });
+  });
+
+  describe('selectLpFormAMM', () => {
+    it('should return the AMM object from the state', () => {
+      const mockState = {
+        lpForm: {
+          amm: {
+            address: '0x123456789abcdef',
+            name: 'Test AMM',
+          },
+          submitButton: {
+            state: 'swap',
+            disabled: false,
+            message: {
+              text: null,
+              isError: false,
+            },
+          },
+        },
+      } as never;
+
+      const result = selectLpFormAMM(mockState);
+
+      expect(result).toEqual({
+        address: '0x123456789abcdef',
+        name: 'Test AMM',
       });
     });
   });
