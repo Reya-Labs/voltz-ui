e7fcf7f79461aa8cb02fc55ac4e1d80fc9946dc6
ssbarbee <ssbarbee@users.noreply.github.com>
2023-04-25 12:01:34 +0200

    refactor: move

---
+++ src/app/features/forms/lp-form/utils/updateLeverageOptionsAfterGetInfoPostLp/index.ts
@@ -0,0 +1,29 @@
+import { Draft } from '@reduxjs/toolkit';
+
+import { formatNumber } from '../../../../../../utilities/number';
+import { calculateLeverageOptions } from '../../../common/utils';
+import { SliceState } from '../../reducer';
+import { getProspectiveLpNotional } from '../getProspectiveLpNotional';
+
+export const updateLeverageOptionsAfterGetInfoPostLp = (state: Draft<SliceState>): void => {
+  let maxLeverage = '--';
+  if (
+    !state.userInput.notionalAmount.error &&
+    state.prospectiveLp.infoPostLp.status === 'success'
+  ) {
+    if (state.prospectiveLp.infoPostLp.value.marginRequirement > 0) {
+      maxLeverage = formatNumber(
+        Math.floor(
+          getProspectiveLpNotional(state) / state.prospectiveLp.infoPostLp.value.marginRequirement,
+        ),
+        0,
+        0,
+      );
+    } else {
+      maxLeverage = formatNumber(Math.floor(state.poolLpInfo.maxLeverage), 0, 0);
+    }
+  }
+
+  state.prospectiveLp.leverage.maxLeverage = maxLeverage;
+  state.prospectiveLp.leverage.options = calculateLeverageOptions(maxLeverage);
+};
