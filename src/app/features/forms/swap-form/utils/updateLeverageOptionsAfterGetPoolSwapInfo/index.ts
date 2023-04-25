b2dc01cff6aa76e843d0ff84abc5701d9063a12e
ssbarbee <ssbarbee@users.noreply.github.com>
2023-04-25 10:52:51 +0200

    refactor: move updateLeverageOptionsAfterGetPoolSwapInfo

---
+++ src/app/features/forms/swap-form/utils/updateLeverageOptionsAfterGetPoolSwapInfo/index.ts
@@ -0,0 +1,16 @@
+import { Draft } from '@reduxjs/toolkit';
+
+import { formatNumber } from '../../../../../../utilities/number';
+import { calculateLeverageOptions } from '../../../common/utils';
+import { SliceState } from '../../reducer';
+import { getProspectiveSwapMode } from '../getProspectiveSwapMode';
+
+export const updateLeverageOptionsAfterGetPoolSwapInfo = (state: Draft<SliceState>): void => {
+  const maxLeverage = formatNumber(
+    Math.floor(state.poolSwapInfo.maxLeverage[getProspectiveSwapMode(state)]),
+    0,
+    0,
+  );
+  state.prospectiveSwap.leverage.maxLeverage = maxLeverage;
+  state.prospectiveSwap.leverage.options = calculateLeverageOptions(maxLeverage);
+};
