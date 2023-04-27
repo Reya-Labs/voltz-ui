2d5bbd8412c1b245256afb2c10e754dec7f62697
ssbarbee <ssbarbee@users.noreply.github.com>
2023-04-27 11:24:54 +0200

    test: add test case for getUnderlyingTokenAllowanceThunk in swap-form reducer

--- src/app/features/forms/swap-form/reducer.test.ts
+++ src/app/features/forms/swap-form/reducer.test.ts
@@ -1,6 +1,6 @@
 import { swapFormReducer } from './reducer';
 import { initialState, SliceState } from './state';
-import { getWalletBalanceThunk } from './thunks';
+import { getUnderlyingTokenAllowanceThunk, getWalletBalanceThunk } from './thunks';
 import { validateUserInputAndUpdateSubmitButton } from './utils';
 
 jest.mock('./utils', () => ({
@@ -41,6 +41,34 @@ describe('swapFormReducer', () => {
       expect(validateUserInputAndUpdateSubmitButton).toHaveBeenCalledTimes(1);
       expect(nextState.walletBalance.status).toEqual('success');
       expect(nextState.walletBalance.value).toEqual(10);
+    });
+  });
+
+  describe('getUnderlyingTokenAllowanceThunk', () => {
+    it('should update status to "pending" when getUnderlyingTokenAllowanceThunk is pending', () => {
+      const nextState = swapFormReducer(testsInitialState, {
+        type: getUnderlyingTokenAllowanceThunk.pending.type,
+      });
+      expect(nextState.walletTokenAllowance.status).toEqual('pending');
+    });
+
+    it('should update status to "error" and set walletTokenAllowance to error state when getUnderlyingTokenAllowanceThunk is rejected', () => {
+      const nextState = swapFormReducer(testsInitialState, {
+        type: getUnderlyingTokenAllowanceThunk.rejected.type,
+      });
+      expect(nextState.walletTokenAllowance.status).toEqual('error');
+      expect(nextState.walletTokenAllowance.value).toEqual(0);
+    });
+
+    it('should update walletTokenAllowance and status to "success" when getUnderlyingTokenAllowanceThunk is fulfilled, also make sure validateUserInputAndUpdateSubmitButton is called', () => {
+      const nextState = swapFormReducer(testsInitialState, {
+        type: getUnderlyingTokenAllowanceThunk.fulfilled.type,
+        payload: 10,
+      });
+
+      expect(validateUserInputAndUpdateSubmitButton).toHaveBeenCalledTimes(1);
+      expect(nextState.walletTokenAllowance.status).toEqual('success');
+      expect(nextState.walletTokenAllowance.value).toEqual(10);
     });
   });
 });
