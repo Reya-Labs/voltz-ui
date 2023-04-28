d8fc5420ff42f170e1f901a1af55a4959cfd43d1
Ioana Roceanu <ioanaroceanu@gmail.com>
2023-04-28 23:34:42 +0100

    feat: custom maturity window

---
+++ src/utilities/maturityWindow.ts
@@ -0,0 +1,8 @@
+export const MATURITY_WINDOW = 1 * 60 * 60 * 1000;
+export const MATURITY_WINDOW_AAVE_LEND = 1 * 60 * 60 * 1000;
+
+export const getMaturityWindow = (protocolId: number) => {
+    return (
+        protocolId === 1 ? MATURITY_WINDOW_AAVE_LEND : MATURITY_WINDOW
+    );
+};
