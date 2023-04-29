7a3f7cb0c4654833ad08d0f2d923392a34798a36
Ioana Roceanu <ioanaroceanu@gmail.com>
2023-04-29 11:47:27 +0100

    fix: maturity 1 day aave lend

--- src/utilities/maturityWindow.ts
+++ src/utilities/maturityWindow.ts
@@ -1,5 +1,5 @@
 export const MATURITY_WINDOW = 1 * 60 * 60 * 1000;
-export const MATURITY_WINDOW_AAVE_LEND = 1 * 60 * 60 * 1000;
+export const MATURITY_WINDOW_AAVE_LEND = 24 * 60 * 60 * 1000;
 
 export const getMaturityWindow = (protocolId: number) => {
   return protocolId === 1 ? MATURITY_WINDOW_AAVE_LEND : MATURITY_WINDOW;
