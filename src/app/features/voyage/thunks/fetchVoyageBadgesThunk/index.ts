c609078a263b5ba1fa12f60f6a61d6d1f787c0c1
ssbarbee <ssbarbee@users.noreply.github.com>
2023-05-19 11:58:31 +0200

    fix: add voyage API integration

--- src/app/features/voyage/thunks/fetchVoyageBadgesThunk/index.ts
+++ src/app/features/voyage/thunks/fetchVoyageBadgesThunk/index.ts
@@ -1,27 +1,17 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { SupportedChainId } from '@voltz-protocol/v1-sdk';
+import { getVoyages, SupportedChainId, Voyage } from '@voltz-protocol/v1-sdk';
 
 import { rejectThunkWithError } from '../../../helpers/reject-thunk-with-error';
 
-export type VoyageBadges = {
-  timestamp: number | undefined;
-};
-
 export const fetchVoyageBadgesThunk = createAsyncThunk<
-  Awaited<VoyageBadges[] | ReturnType<typeof rejectThunkWithError>>,
+  Awaited<Voyage[] | ReturnType<typeof rejectThunkWithError>>,
   {
     chainId: SupportedChainId;
     account: string;
   }
 >('voyage/fetchBadges', async ({ account, chainId }, thunkAPI) => {
   try {
-    const promise = new Promise((resolve) => setTimeout(resolve, Math.random() * 1000));
-    await promise;
-    return [
-      {
-        timestamp: Math.random() * 100 > 50 ? undefined : new Date().valueOf(),
-      },
-    ];
+    return await getVoyages({ chainId, account });
   } catch (err) {
     return rejectThunkWithError(thunkAPI, err);
   }
