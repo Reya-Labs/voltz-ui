de8ee1034919f24e195f6d885a950a5941c3cdc9
ssbarbee <ssbarbee@users.noreply.github.com>
2023-05-23 13:14:22 +0300

    fix: remove flagged variable

--- src/utilities/isEnvVarProvided/is-settle-flow-enabled.ts
+++
@@ -1,4 +0,0 @@
-import { isEnvVarProvided } from './index';
-
-export const isSettleFlowEnabled = () =>
-  isEnvVarProvided(process.env.REACT_APP_SETTLE_FLOW_ENABLED);
--- src/utilities/isEnvVarProvided/is-trader-rollover-experience-flow-enabled.ts
+++
@@ -1,4 +0,0 @@
-import { isEnvVarProvided } from './index';
-
-export const isTraderRolloverExperienceFlowEnabled = () =>
-  isEnvVarProvided(process.env.REACT_APP_TRADER_ROLLOVER_EXPERIENCE_FLOW_ENABLED);
