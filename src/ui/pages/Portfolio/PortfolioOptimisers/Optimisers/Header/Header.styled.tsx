3c5d45416671dd958cc939665a9c0d9e4e243719
ssbarbee <ssbarbee@users.noreply.github.com>
2023-05-30 10:25:03 +0200

    fix: styling for Header

--- src/ui/pages/Portfolio/PortfolioOptimisers/Optimisers/Header/Header.styled.tsx
+++ src/ui/pages/Portfolio/PortfolioOptimisers/Optimisers/Header/Header.styled.tsx
@@ -1,10 +1,13 @@
 import styled from '@emotion/styled';
-import { colors } from 'brokoli-ui';
 
 export const HeaderBox = styled('div')`
   display: flex;
   flex-direction: row;
-  /* Liberty 6 */
-  border-bottom: 1px solid ${colors.liberty6};
-  padding: 0px 24px 0px 38px;
+  justify-content: space-between;
+`;
+
+export const RightBox = styled('div')`
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
 `;
--- src/ui/pages/Portfolio/PortfolioOptimisers/Optimisers/Header/index.tsx
+++ src/ui/pages/Portfolio/PortfolioOptimisers/Optimisers/Header/index.tsx
@@ -1,18 +1,20 @@
 import { Typography } from 'brokoli-ui';
 import React from 'react';
 
-import { HeaderBox } from './Header.styled';
+import { HeaderBox, RightBox } from './Header.styled';
 
 export const Header: React.FunctionComponent = React.memo(() => (
   <HeaderBox data-testid="Header-HeaderBox">
     <Typography colorToken="lavenderWeb3" typographyToken="primaryBodyXSmallRegular">
       Vault
     </Typography>
-    <Typography colorToken="lavenderWeb3" typographyToken="primaryBodyXSmallRegular">
-      Current Balance
-    </Typography>
-    <Typography colorToken="lavenderWeb3" typographyToken="primaryBodyXSmallRegular">
-      Automatic Rollover
-    </Typography>
+    <RightBox>
+      <Typography colorToken="lavenderWeb3" typographyToken="primaryBodyXSmallRegular">
+        Current Balance
+      </Typography>
+      <Typography colorToken="lavenderWeb3" typographyToken="primaryBodyXSmallRegular">
+        Automatic Rollover
+      </Typography>
+    </RightBox>
   </HeaderBox>
 ));
