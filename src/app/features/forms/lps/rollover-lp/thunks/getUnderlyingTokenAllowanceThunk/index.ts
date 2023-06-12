6c07c43ff24de9a445754d633b59084d82da052a
ssbarbee <ssbarbee@users.noreply.github.com>
2023-06-12 15:28:00 +0200

    feat: add getAllowanceToPeriphery for lpRollover,swapRollover integration

--- src/app/features/forms/lps/rollover-lp/thunks/getUnderlyingTokenAllowanceThunk/index.ts
+++ src/app/features/forms/lps/rollover-lp/thunks/getUnderlyingTokenAllowanceThunk/index.ts
@@ -1,8 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { getAllowanceToPeriphery } from '@voltz-protocol/sdk-v1-stateless';
 import { SupportedChainId } from '@voltz-protocol/v1-sdk';
 
 import { getAlchemyKey } from '../../../../../../../utilities/getAlchemyKey';
 import { getInfuraKey } from '../../../../../../../utilities/getInfuraKey';
+import { isV1StatelessEnabled } from '../../../../../../../utilities/isEnvVarProvided/is-v1-stateless-enabled';
 import { RootState } from '../../../../../../store';
 import { rejectThunkWithError } from '../../../../../helpers/reject-thunk-with-error';
 
@@ -16,13 +18,19 @@ export const getUnderlyingTokenAllowanceThunk = createAsyncThunk<
     if (!amm || !amm.signer) {
       return;
     }
-
-    return await amm.getUnderlyingTokenAllowance({
-      forceErc20Check: false,
-      chainId,
-      alchemyApiKey: getAlchemyKey(),
-      infuraApiKey: getInfuraKey(),
-    });
+    if (isV1StatelessEnabled()) {
+      return await getAllowanceToPeriphery({
+        ammId: amm.id,
+        signer: amm.signer,
+      });
+    } else {
+      return await amm.getUnderlyingTokenAllowance({
+        forceErc20Check: false,
+        chainId,
+        alchemyApiKey: getAlchemyKey(),
+        infuraApiKey: getInfuraKey(),
+      });
+    }
   } catch (err) {
     return rejectThunkWithError(thunkAPI, err);
   }
