29e77047c4c73354dc618d9b7d562570f501b107
ssbarbee <ssbarbee@users.noreply.github.com>
2023-06-12 15:33:53 +0200

    feat: add getPoolLpInfo for lpRollover integration

--- src/app/features/forms/lps/rollover-lp/utils/getDefaultLpFixedLow/index.ts
+++ src/app/features/forms/lps/rollover-lp/utils/getDefaultLpFixedLow/index.ts
@@ -4,7 +4,7 @@ import { SliceState } from '../../state';
 
 export const getDefaultLpFixedLow = (state: Draft<SliceState>): number => {
   if (!state.previousPosition) {
-    // todo: layer in smarter dynamic default range logic in here
+    // todo: Artur layer in smarter dynamic default range logic in here
     return 1;
   }
   return state.previousPosition.fixedRateLower.toNumber();
