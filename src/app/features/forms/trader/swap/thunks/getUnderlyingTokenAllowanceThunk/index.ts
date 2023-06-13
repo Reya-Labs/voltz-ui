a8a493e2d4a29d34c1976bc5328fee1b3c786169
ssbarbee <ssbarbee@users.noreply.github.com>
2023-06-13 23:12:04 +0200

    feat: add getAllowanceToPeriphery integrations for sdk-v2

--- src/app/features/forms/trader/swap/thunks/getUnderlyingTokenAllowanceThunk/index.ts
+++ src/app/features/forms/trader/swap/thunks/getUnderlyingTokenAllowanceThunk/index.ts
@@ -1,7 +1,9 @@
 import { AsyncThunkPayloadCreator, createAsyncThunk } from '@reduxjs/toolkit';
 import { getAllowanceToPeriphery } from '@voltz-protocol/sdk-v1-stateless';
+import { getAllowanceToPeriphery as getAllowanceToPeripheryV2 } from '@voltz-protocol/sdk-v2';
 import { SupportedChainId } from '@voltz-protocol/v1-sdk';
 
+import { isV2AMM } from '../../../../../../../utilities/amm';
 import { getAlchemyKey } from '../../../../../../../utilities/getAlchemyKey';
 import { getInfuraKey } from '../../../../../../../utilities/getInfuraKey';
 import { isV1StatelessEnabled } from '../../../../../../../utilities/isEnvVarProvided/is-v1-stateless-enabled';
@@ -19,18 +21,25 @@ export const getUnderlyingTokenAllowanceThunkHandler: AsyncThunkPayloadCreator<
   }
 
   try {
-    if (isV1StatelessEnabled()) {
-      return await getAllowanceToPeriphery({
+    if (isV2AMM(amm)) {
+      return await getAllowanceToPeripheryV2({
         ammId: amm.id,
         signer: amm.signer,
       });
     } else {
-      return await amm.getUnderlyingTokenAllowance({
-        forceErc20Check: false,
-        chainId,
-        alchemyApiKey: getAlchemyKey(),
-        infuraApiKey: getInfuraKey(),
-      });
+      if (isV1StatelessEnabled()) {
+        return await getAllowanceToPeriphery({
+          ammId: amm.id,
+          signer: amm.signer,
+        });
+      } else {
+        return await amm.getUnderlyingTokenAllowance({
+          forceErc20Check: false,
+          chainId,
+          alchemyApiKey: getAlchemyKey(),
+          infuraApiKey: getInfuraKey(),
+        });
+      }
     }
   } catch (err) {
     return rejectThunkWithError(thunkAPI, err);
