749418272702cc9978712260f49b0d8447a6f99c
ssbarbee <ssbarbee@users.noreply.github.com>
2023-06-13 22:28:44 +0200

    feat: add getBalance integrations for sdk-v2

--- src/app/features/forms/lps/rollover-lp/thunks/getWalletBalanceThunk/index.ts
+++ src/app/features/forms/lps/rollover-lp/thunks/getWalletBalanceThunk/index.ts
@@ -1,6 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { getBalance } from '@voltz-protocol/sdk-v1-stateless';
+import { getBalance as getBalanceV2 } from '@voltz-protocol/sdk-v2';
 
+import { isV2AMM } from '../../../../../../../utilities/amm';
 import { isV1StatelessEnabled } from '../../../../../../../utilities/isEnvVarProvided/is-v1-stateless-enabled';
 import { RootState } from '../../../../../../store';
 import { rejectThunkWithError } from '../../../../../helpers/reject-thunk-with-error';
@@ -15,13 +17,20 @@ export const getWalletBalanceThunk = createAsyncThunk<
     if (!amm || !amm.signer) {
       return 0;
     }
-    if (isV1StatelessEnabled()) {
-      return await getBalance({
+    if (isV2AMM(amm)) {
+      return await getBalanceV2({
         ammId: amm.id,
         signer: amm.signer,
       });
     } else {
-      return await amm.underlyingTokens();
+      if (isV1StatelessEnabled()) {
+        return await getBalance({
+          ammId: amm.id,
+          signer: amm.signer,
+        });
+      } else {
+        return await amm.underlyingTokens();
+      }
     }
   } catch (err) {
     return rejectThunkWithError(thunkAPI, err);
