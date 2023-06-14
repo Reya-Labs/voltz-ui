30334f1eb6354b6cbc50793380d0ba2eacc5db41
Ioana Roceanu <ioanaroceanu@gmail.com>
2023-06-14 20:55:26 +0100

    chore: mock api for access badges

--- src/app/features/voyage/thunks/fetchVoyageBadgesThunk/index.ts
+++ src/app/features/voyage/thunks/fetchVoyageBadgesThunk/index.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { getVoyages, SupportedChainId, Voyage } from '@voltz-protocol/v1-sdk';
+import { SupportedChainId, Voyage } from '@voltz-protocol/v1-sdk';
 
 import { rejectThunkWithError } from '../../../helpers/reject-thunk-with-error';
 
@@ -11,8 +11,75 @@ export const fetchVoyageBadgesThunk = createAsyncThunk<
   }
 >('voyage/fetchBadges', async ({ account, chainId }, thunkAPI) => {
   try {
-    return await getVoyages({ chainId, account });
+    return getVoyages({ chainId, account });
   } catch (err) {
     return rejectThunkWithError(thunkAPI, err);
   }
 });
+
+function getVoyages(info: { chainId: number; account: string }): Voyage[] {
+  if (info.account === '0x2fa11ef008c4b585ccf0a76861794ac7ae5a3a67') {
+    return [
+      {
+        id: 1,
+        status: 'achieved',
+        timestamp: 1686758231,
+      },
+      {
+        id: 2,
+        status: 'achieved',
+        timestamp: 1686758231,
+      },
+      {
+        id: 3,
+        status: 'achieved',
+        timestamp: 1686758231,
+      },
+      {
+        id: 4,
+        status: 'achieved',
+        timestamp: 1686758231,
+      },
+    ];
+  } else if (info.account === '0x45556408e543158f74403e882e3c8c23ecd9f732') {
+    return [
+      {
+        id: 1,
+        status: 'achieved',
+        timestamp: 1686758231,
+      },
+    ];
+  } else if (info.account === '0xf8f6b70a36f4398f0853a311dc6699aba8333cc1') {
+    return [
+      {
+        id: 1,
+        status: 'achieved',
+        timestamp: 1686758231,
+      },
+      {
+        id: 2,
+        status: 'achieved',
+        timestamp: 1686758231,
+      },
+      {
+        id: 3,
+        status: 'achieved',
+        timestamp: 1686758231,
+      },
+    ];
+  } else if (info.account === '0x5173cc04a12d1fc7c83293ed71fbfa6bc0f87739') {
+    return [
+      {
+        id: 1,
+        status: 'achieved',
+        timestamp: 1686758231,
+      },
+      {
+        id: 2,
+        status: 'achieved',
+        timestamp: 1686758231,
+      },
+    ];
+  }
+  return [];
+}
