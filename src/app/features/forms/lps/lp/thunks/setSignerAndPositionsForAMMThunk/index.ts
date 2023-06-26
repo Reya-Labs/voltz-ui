8ec0967462f795f29e2058c9f2f567f8f304a0c1
tudor47 <tudorcostin7@gmail.com>
2023-06-26 11:24:38 +0300

    feat: updating sdks

--- src/app/features/forms/lps/lp/thunks/setSignerAndPositionsForAMMThunk/index.ts
+++ src/app/features/forms/lps/lp/thunks/setSignerAndPositionsForAMMThunk/index.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { getPositions, Position, SupportedChainId } from '@voltz-protocol/v1-sdk';
+import { getLpPositionByPool, Position, SupportedChainId } from '@voltz-protocol/v1-sdk';
 import { providers } from 'ethers';
 
 import { findCurrentPositionsLp } from '../../../../../../../utilities/amm';
@@ -40,24 +40,7 @@ export const setSignerAndPositionsForAMMThunk = createAsyncThunk<
     }
 
     const userWalletId = (await signer.getAddress()).toLowerCase();
-
-    const { positions, error } = await getPositions({
-      chainId,
-      userWalletId: userWalletId,
-      amms: [amm],
-      type: 'LP',
-    });
-
-    if (error) {
-      return rejectThunkWithError(thunkAPI, error);
-    }
-
-    if (positions.length === 0) {
-      return {
-        signer: signer,
-        positions: positions,
-      };
-    }
+    const positions = await getLpPositionByPool(amm.id, userWalletId, amm);
 
     const filteredPositions: Position[] = findCurrentPositionsLp(positions || [], amm.id) || null;
     return {
