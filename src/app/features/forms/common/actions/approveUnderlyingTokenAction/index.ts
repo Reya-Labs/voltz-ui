c00af4a11570c929b81ea7d6a31b1369fbdaffcc
ssbarbee <ssbarbee@users.noreply.github.com>
2023-06-26 13:47:25 +0200

    refactor: rename

--- src/app/features/forms/common/actions/approveUnderlyingTokenAction/index.ts
+++ src/app/features/forms/common/actions/approveUnderlyingTokenAction/index.ts
@@ -6,7 +6,7 @@ import { Signer } from 'ethers';
 import { isV2AMM } from '../../../../../../utilities/amm';
 import { isV1StatelessEnabled } from '../../../../../../utilities/isEnvVarProvided/is-v1-stateless-enabled';
 
-export const approveUnderlyingToken = async ({
+export const approveUnderlyingTokenAction = async ({
   amm,
   signer,
 }: {
