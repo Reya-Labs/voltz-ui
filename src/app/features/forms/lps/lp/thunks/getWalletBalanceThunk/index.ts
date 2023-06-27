0ce0e9b9fbb798f34b6d5bfc4f7e08a178a13b56
ssbarbee <ssbarbee@users.noreply.github.com>
2023-06-27 11:30:18 +0200

    feat: resuse getWalletBalanceService

--- src/app/features/forms/lps/lp/thunks/getWalletBalanceThunk/index.ts
+++ src/app/features/forms/lps/lp/thunks/getWalletBalanceThunk/index.ts
@@ -1,39 +1,27 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { getBalance } from '@voltz-protocol/sdk-v1-stateless';
-import { getBalance as getBalanceV2 } from '@voltz-protocol/sdk-v2';
+import { AsyncThunkPayloadCreator, createAsyncThunk } from '@reduxjs/toolkit';
 
-import { isV2AMM } from '../../../../../../../utilities/amm';
-import { isV1StatelessEnabled } from '../../../../../../../utilities/isEnvVarProvided/is-v1-stateless-enabled';
 import { RootState } from '../../../../../../store';
 import { rejectThunkWithError } from '../../../../../helpers/reject-thunk-with-error';
+import { getWalletBalanceService } from '../../../../common';
 
-// TODO: FB same as in swap-form
+export const getWalletBalanceThunkHandler: AsyncThunkPayloadCreator<
+  Awaited<number | ReturnType<typeof rejectThunkWithError>>,
+  void,
+  { state: RootState }
+> = async (_, thunkAPI) => {
+  try {
+    const amm = thunkAPI.getState().lpForm.amm;
+    return await getWalletBalanceService({
+      amm: amm!,
+      signer: amm!.signer!,
+    });
+  } catch (err) {
+    return rejectThunkWithError(thunkAPI, err);
+  }
+};
+
 export const getWalletBalanceThunk = createAsyncThunk<
   Awaited<number | ReturnType<typeof rejectThunkWithError>>,
   void,
   { state: RootState }
->('lpForm/getWalletBalance', async (_, thunkAPI) => {
-  try {
-    const amm = thunkAPI.getState().lpForm.amm;
-    if (!amm || !amm.signer) {
-      return 0;
-    }
-    if (isV2AMM(amm)) {
-      return await getBalanceV2({
-        ammId: amm.id,
-        signer: amm.signer,
-      });
-    } else {
-      if (isV1StatelessEnabled()) {
-        return await getBalance({
-          ammId: amm.id,
-          signer: amm.signer,
-        });
-      } else {
-        return await amm.underlyingTokens();
-      }
-    }
-  } catch (err) {
-    return rejectThunkWithError(thunkAPI, err);
-  }
-});
+>('lpForm/getWalletBalance', getWalletBalanceThunkHandler);
