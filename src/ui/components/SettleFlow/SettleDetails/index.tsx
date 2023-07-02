a7eb9883e6bba3c23973e8043c04527ebec3f29c
Artur Begyan <arturbegyan98@gmail.com>
2023-07-02 18:45:22 +0100

    feat: settle flow glp

--- src/ui/components/SettleFlow/SettleDetails/index.tsx
+++ src/ui/components/SettleFlow/SettleDetails/index.tsx
@@ -12,6 +12,7 @@ import {
   selectFixedLower,
   selectFixedUpper,
   selectSettleVariant,
+  selectIsGLP28June
 } from '../../../../app/features/settle-flow';
 import { useAppSelector } from '../../../../app/hooks';
 import { RealizedPNLDetails } from '../../RealizedPNLDetails';
@@ -28,129 +29,142 @@ export const SettleDetails: React.FunctionComponent = () => {
   const fixedUpper = useAppSelector(selectFixedUpper);
   const variant = useAppSelector(selectSettleVariant);
   const token = useAppSelector(selectAMMToken);
+  const isGLP28June = useAppSelector(selectIsGLP28June);
 
   if (!variant || !token) {
     return null;
   }
 
-  return (
-    <DetailsBox>
-      <DetailBox>
-        <Typography colorToken="lavenderWeb3" typographyToken="primaryBodySmallRegular">
-          Notional
-        </Typography>
-        <TokenTypography
-          colorToken="lavenderWeb"
-          token={`${compactNotional.compactNotionalSuffix}${tokenFormatted}`}
-          typographyToken="secondaryBodySmallRegular"
-          value={compactNotional.compactNotionalNumber}
-        />
-      </DetailBox>
-      {variant === 'lp' ? (
-        <React.Fragment>
-          <DetailBox>
-            <Typography colorToken="lavenderWeb3" typographyToken="primaryBodySmallRegular">
-              Fixed Rate Low
-            </Typography>
-            <TokenTypography
-              colorToken="lavenderWeb"
-              token="%"
-              typographyToken="secondaryBodySmallRegular"
-              value={fixedLower || 0}
-            />
-          </DetailBox>
-          <DetailBox>
-            <Typography colorToken="lavenderWeb3" typographyToken="primaryBodySmallRegular">
-              Fixed Rate High
-            </Typography>
-            <TokenTypography
-              colorToken="lavenderWeb"
-              token="%"
-              typographyToken="secondaryBodySmallRegular"
-              value={fixedUpper || 0}
-            />
-          </DetailBox>
-        </React.Fragment>
-      ) : null}
-      <DetailBox>
-        <TypographyWithTooltip
-          colorToken="lavenderWeb3"
-          tooltip="Deposited Margin = Margin Provided at any time - Margin Removed at any time"
-          tooltipColorToken="lavenderWeb2"
-          typographyToken="primaryBodySmallRegular"
-        >
-          Deposited Margin
-        </TypographyWithTooltip>
-        <TokenTypography
-          colorToken="lavenderWeb"
-          token={`${compactDepositedMargin.compactDepositedMarginSuffix}${tokenFormatted}`}
-          typographyToken="secondaryBodySmallRegular"
-          value={compactDepositedMargin.compactDepositedMarginNumber}
-        />
-      </DetailBox>
-      <DetailBox>
-        <TypographyWithTooltip
-          colorToken="lavenderWeb3"
-          tooltip={
-            <RealizedPNLDetails
-              pnlFromFees={compactRealizedPnL.compactRealizedPnLFees}
-              pnlFromSwaps={compactRealizedPnL.compactRealizedPnLSwaps}
-              pnlTotal={`${compactRealizedPnL.compactRealizedPnLTotal} ${compactRealizedPnL.compactRealizedPnLSuffix}`}
-              underlyingTokenName={token}
-              variant={variant}
-            />
-          }
-          tooltipColorToken="lavenderWeb2"
-          typographyToken="primaryBodySmallRegular"
-        >
-          Realized PnL
-        </TypographyWithTooltip>
-        <TokenTypography
-          colorToken={
-            compactRealizedPnL.compactRealizedPnLTotal === '--'
-              ? 'lavenderWeb'
-              : compactRealizedPnL.compactRealizedPnLTotal.indexOf('-') !== -1
-              ? 'wildStrawberry'
-              : 'skyBlueCrayola'
-          }
-          token={`${compactRealizedPnL.compactRealizedPnLSuffix}${tokenFormatted}`}
-          typographyToken="secondaryBodySmallRegular"
-          value={compactRealizedPnL.compactRealizedPnLTotal}
-        />
-      </DetailBox>
-      <DetailBox>
-        <TypographyWithTooltip
-          colorToken="lavenderWeb3"
-          tooltip="Settling Balance = Deposited Margin + Realized PnL"
-          tooltipColorToken="lavenderWeb2"
-          typographyToken="primaryBodySmallRegular"
-        >
-          Settling Balance
-        </TypographyWithTooltip>
-        <TokenTypography
-          colorToken={
-            compactNetBalance.compactNetBalanceNumber === '--'
-              ? 'lavenderWeb'
-              : compactNetBalance.compactNetBalanceNumber.indexOf('-') !== -1
-              ? 'wildStrawberry'
-              : 'skyBlueCrayola'
-          }
-          token={`${compactNetBalance.compactNetBalanceSuffix}${tokenFormatted}`}
-          typographyToken="secondaryBodySmallRegular"
-          value={compactNetBalance.compactNetBalanceNumber}
-        />
-      </DetailBox>
-      <DetailBox>
-        <Typography colorToken="lavenderWeb3" typographyToken="primaryBodySmallRegular">
-          Maturity
-        </Typography>
-        <TokenTypography
-          colorToken="lavenderWeb"
-          token=" "
-          typographyToken="secondaryBodySmallRegular"
-          value={aMMMaturity}
-        />
-      </DetailBox>
-    </DetailsBox>
-  );
+  if (!isGLP28June) {
+    return (
+      <DetailsBox>
+        <DetailBox>
+          <Typography colorToken="lavenderWeb3" typographyToken="primaryBodySmallRegular">
+            Notional
+          </Typography>
+          <TokenTypography
+            colorToken="lavenderWeb"
+            token={`${compactNotional.compactNotionalSuffix}${tokenFormatted}`}
+            typographyToken="secondaryBodySmallRegular"
+            value={compactNotional.compactNotionalNumber}
+          />
+        </DetailBox>
+        {variant === 'lp' ? (
+          <React.Fragment>
+            <DetailBox>
+              <Typography colorToken="lavenderWeb3" typographyToken="primaryBodySmallRegular">
+                Fixed Rate Low
+              </Typography>
+              <TokenTypography
+                colorToken="lavenderWeb"
+                token="%"
+                typographyToken="secondaryBodySmallRegular"
+                value={fixedLower || 0}
+              />
+            </DetailBox>
+            <DetailBox>
+              <Typography colorToken="lavenderWeb3" typographyToken="primaryBodySmallRegular">
+                Fixed Rate High
+              </Typography>
+              <TokenTypography
+                colorToken="lavenderWeb"
+                token="%"
+                typographyToken="secondaryBodySmallRegular"
+                value={fixedUpper || 0}
+              />
+            </DetailBox>
+          </React.Fragment>
+        ) : null}
+        <DetailBox>
+          <TypographyWithTooltip
+            colorToken="lavenderWeb3"
+            tooltip="Deposited Margin = Margin Provided at any time - Margin Removed at any time"
+            tooltipColorToken="lavenderWeb2"
+            typographyToken="primaryBodySmallRegular"
+          >
+            Deposited Margin
+          </TypographyWithTooltip>
+          <TokenTypography
+            colorToken="lavenderWeb"
+            token={`${compactDepositedMargin.compactDepositedMarginSuffix}${tokenFormatted}`}
+            typographyToken="secondaryBodySmallRegular"
+            value={compactDepositedMargin.compactDepositedMarginNumber}
+          />
+        </DetailBox>
+        <DetailBox>
+          <TypographyWithTooltip
+            colorToken="lavenderWeb3"
+            tooltip={
+              <RealizedPNLDetails
+                pnlFromFees={compactRealizedPnL.compactRealizedPnLFees}
+                pnlFromSwaps={compactRealizedPnL.compactRealizedPnLSwaps}
+                pnlTotal={`${compactRealizedPnL.compactRealizedPnLTotal} ${compactRealizedPnL.compactRealizedPnLSuffix}`}
+                underlyingTokenName={token}
+                variant={variant}
+              />
+            }
+            tooltipColorToken="lavenderWeb2"
+            typographyToken="primaryBodySmallRegular"
+          >
+            Realized PnL
+          </TypographyWithTooltip>
+          <TokenTypography
+            colorToken={
+              compactRealizedPnL.compactRealizedPnLTotal === '--'
+                ? 'lavenderWeb'
+                : compactRealizedPnL.compactRealizedPnLTotal.indexOf('-') !== -1
+                  ? 'wildStrawberry'
+                  : 'skyBlueCrayola'
+            }
+            token={`${compactRealizedPnL.compactRealizedPnLSuffix}${tokenFormatted}`}
+            typographyToken="secondaryBodySmallRegular"
+            value={compactRealizedPnL.compactRealizedPnLTotal}
+          />
+        </DetailBox>
+        <DetailBox>
+          <TypographyWithTooltip
+            colorToken="lavenderWeb3"
+            tooltip="Settling Balance = Deposited Margin + Realized PnL"
+            tooltipColorToken="lavenderWeb2"
+            typographyToken="primaryBodySmallRegular"
+          >
+            Settling Balance
+          </TypographyWithTooltip>
+          <TokenTypography
+            colorToken={
+              compactNetBalance.compactNetBalanceNumber === '--'
+                ? 'lavenderWeb'
+                : compactNetBalance.compactNetBalanceNumber.indexOf('-') !== -1
+                  ? 'wildStrawberry'
+                  : 'skyBlueCrayola'
+            }
+            token={`${compactNetBalance.compactNetBalanceSuffix}${tokenFormatted}`}
+            typographyToken="secondaryBodySmallRegular"
+            value={compactNetBalance.compactNetBalanceNumber}
+          />
+        </DetailBox>
+        <DetailBox>
+          <Typography colorToken="lavenderWeb3" typographyToken="primaryBodySmallRegular">
+            Maturity
+          </Typography>
+          <TokenTypography
+            colorToken="lavenderWeb"
+            token=" "
+            typographyToken="secondaryBodySmallRegular"
+            value={aMMMaturity}
+          />
+        </DetailBox>
+      </DetailsBox>
+    );
+  } else {
+    return (
+      <Typography colorToken="lavenderWeb" typographyToken="primaryBodySmallRegular">
+        Warning: Settlement for the GLP pool will take place in line with the decisions made by the community in the following vote: https://www.voltz.xyz/voltz-snapshot
+
+        By settling you are confirming agreement to these settlement terms. [AWAITING LEGAL INPUT]
+      </Typography>
+    )
+  }
+
+
 };
