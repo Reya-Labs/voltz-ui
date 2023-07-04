567cef027706cb145dde7f05f203b7d6f229894e
ssbarbee <ssbarbee@users.noreply.github.com>
2023-07-04 10:25:28 +0200

    fix: rollover swap white screen

--- src/app/features/forms/trader/rollover-swap/reducer.test.ts
+++ src/app/features/forms/trader/rollover-swap/reducer.test.ts
@@ -385,10 +385,10 @@ describe('rolloverSwapFormReducer', () => {
         const nextState = rolloverSwapFormReducer(testsInitialState, {
           type: getPoolSwapInfoThunk.fulfilled.type,
           payload: {
-            availableNotionalFT: 1,
-            availableNotionalVT: 2,
-            maxLeverageFT: 3,
-            maxLeverageVT: 4,
+            availableNotionalFixedTaker: 1,
+            availableNotionalVariableTaker: 2,
+            maxLeverageFixedTaker: 3,
+            maxLeverageVariableTaker: 4,
           },
         });
 
