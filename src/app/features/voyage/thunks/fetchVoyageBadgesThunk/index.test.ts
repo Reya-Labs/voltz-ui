8492fecc27a029a5e209601c957337da3b6f0cc5
ssbarbee <ssbarbee@users.noreply.github.com>
2023-07-05 10:26:54 +0200

    test: add tests for fetchVoyageBadgesThunk

---
+++ src/app/features/voyage/thunks/fetchVoyageBadgesThunk/index.test.ts
@@ -0,0 +1,48 @@
+import { getVoyages } from '@voltz-protocol/v1-sdk';
+
+import { rejectThunkWithError } from '../../../helpers/reject-thunk-with-error';
+import { fetchVoyageBadgesThunkHandler } from '.';
+
+jest.mock('@voltz-protocol/v1-sdk');
+jest.mock('../../../helpers/reject-thunk-with-error');
+
+describe('fetchVoyageBadgesThunkHandler', () => {
+  const mockAccount = '0x123';
+  const mockChainId = '1';
+  const mockVoyages = [{ id: '1' }, { id: '2' }];
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return the voyages for the given account and chain ID', async () => {
+    (getVoyages as jest.Mock).mockResolvedValue(mockVoyages);
+
+    const result = await fetchVoyageBadgesThunkHandler(
+      {
+        account: mockAccount,
+        chainId: mockChainId,
+      } as never,
+      {} as never,
+    );
+
+    expect(getVoyages).toHaveBeenCalledWith({ chainId: mockChainId, account: mockAccount });
+    expect(result).toEqual(mockVoyages);
+  });
+
+  it('should return a rejected thunk with an error if getVoyages throws an error', async () => {
+    const mockError = new Error('Failed to fetch voyages');
+    (getVoyages as jest.Mock).mockRejectedValue(mockError);
+
+    await fetchVoyageBadgesThunkHandler(
+      {
+        account: mockAccount,
+        chainId: mockChainId,
+      } as never,
+      {} as never,
+    );
+
+    expect(getVoyages).toHaveBeenCalledWith({ chainId: mockChainId, account: mockAccount });
+    expect(rejectThunkWithError).toHaveBeenCalledWith(expect.any(Object), mockError);
+  });
+});
--- src/app/features/voyage/thunks/fetchVoyageBadgesThunk/index.ts
+++ src/app/features/voyage/thunks/fetchVoyageBadgesThunk/index.ts
@@ -1,7 +1,21 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { AsyncThunkPayloadCreator, createAsyncThunk } from '@reduxjs/toolkit';
 import { getVoyages, SupportedChainId, Voyage } from '@voltz-protocol/v1-sdk';
 
 import { rejectThunkWithError } from '../../../helpers/reject-thunk-with-error';
+
+export const fetchVoyageBadgesThunkHandler: AsyncThunkPayloadCreator<
+  Awaited<Voyage[] | ReturnType<typeof rejectThunkWithError>>,
+  {
+    chainId: SupportedChainId;
+    account: string;
+  }
+> = async ({ account, chainId }, thunkAPI) => {
+  try {
+    return await getVoyages({ chainId, account });
+  } catch (err) {
+    return rejectThunkWithError(thunkAPI, err);
+  }
+};
 
 export const fetchVoyageBadgesThunk = createAsyncThunk<
   Awaited<Voyage[] | ReturnType<typeof rejectThunkWithError>>,
@@ -9,10 +23,4 @@ export const fetchVoyageBadgesThunk = createAsyncThunk<
     chainId: SupportedChainId;
     account: string;
   }
->('voyage/fetchBadges', async ({ account, chainId }, thunkAPI) => {
-  try {
-    return await getVoyages({ chainId, account });
-  } catch (err) {
-    return rejectThunkWithError(thunkAPI, err);
-  }
-});
+>('voyage/fetchBadges', fetchVoyageBadgesThunkHandler);
