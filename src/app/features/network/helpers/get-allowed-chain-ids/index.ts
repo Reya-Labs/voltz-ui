eb1cef2df8e131f55ca71be4470e10ed4379d202
ssbarbee <ssbarbee@users.noreply.github.com>
2023-07-06 12:50:51 +0200

    test: add tests for getAllowedChainIds

--- src/app/features/network/helpers/get-allowed-chain-ids/index.ts
+++ src/app/features/network/helpers/get-allowed-chain-ids/index.ts
@@ -1,13 +1,13 @@
 import { SupportedChainId } from '@voltz-protocol/v1-sdk';
 
-import { getChainsFromProcessEnv } from './get-chains-from-process-env';
+import { getChainsFromProcessEnv } from '../get-chains-from-process-env';
 
 type SupportedChainIds = keyof typeof SupportedChainId;
 
 export const getAllowedChainIds = (
   networkConfigurationString = process.env.REACT_APP_NETWORK_SELECTOR_NETWORKS,
 ): SupportedChainId[] => {
-  const allowedNetworks = getChainsFromProcessEnv(networkConfigurationString);
+  const allowedNetworks = getChainsFromProcessEnv(networkConfigurationString) || [];
   return allowedNetworks
     .filter(({ network }) => SupportedChainId[network as SupportedChainIds])
     .map(({ network }) => SupportedChainId[network as SupportedChainIds]);
