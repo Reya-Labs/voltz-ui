82f7d53ce7aefaba319f01e57e69b3be62417bda
ssbarbee <ssbarbee@users.noreply.github.com>
2023-07-26 11:07:19 +0200

    feat: add structure for PortfolioMarginAccounts

---
+++ src/ui/pages/PortfolioMarginAccounts/PortfolioPositions/PositionsList/PositionEntry/Entry/PositionTransactionHistoryDialogContent/PositionTransactionHistoryDialogContent.styled.tsx
@@ -0,0 +1,29 @@
+import styled from '@emotion/styled';
+
+export const ContentBox = styled('div')`
+  display: flex;
+  box-sizing: border-box;
+  width: 550px;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 24px;
+`;
+
+export const TitleBox = styled('div')`
+  box-sizing: border-box;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px;
+`;
+
+export const ButtonsBox = styled('div')`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+  width: 100%;
+`;
---
+++ src/ui/pages/PortfolioMarginAccounts/PortfolioPositions/PositionsList/PositionsHeader/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { PositionsFilterId } from '../index';
+import { ActivePositionsHeader } from './ActivePositionsHeader';
+import { MaturedPositionsHeader } from './MaturedPositionsHeader';
+import { SettledPositionsHeader } from './SettledPositionsHeader';
+
+type PositionsHeaderProps = {
+  positionsFilterId: PositionsFilterId;
+};
+
+export const PositionsHeader: React.FunctionComponent<PositionsHeaderProps> = ({
+  positionsFilterId,
+}) => {
+  if (positionsFilterId === 'matured') {
+    return <MaturedPositionsHeader />;
+  }
+  if (positionsFilterId === 'settled') {
+    return <SettledPositionsHeader />;
+  }
+  return <ActivePositionsHeader />;
+};
