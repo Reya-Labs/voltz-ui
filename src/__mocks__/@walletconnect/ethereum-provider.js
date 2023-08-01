6a7a8fac4c2c1508fd9620e06c8e1dc9e6cdbb14
ssbarbee <ssbarbee@users.noreply.github.com>
2023-08-01 13:27:30 +0200

    fix: broken tests

---
+++ src/__mocks__/@walletconnect/ethereum-provider.js
@@ -0,0 +1,9 @@
+// File: __mocks__/@walletconnect/ethereum-provider.js
+
+// Mocked EthereumProvider class
+export class EthereumProvider {
+  // Add mock implementations of methods you want to use in your tests
+  // For example, you can use jest.fn() to create mock functions.
+}
+
+// Export any other mocked objects or functions if required
