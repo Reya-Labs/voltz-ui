a73ae5cabd8a684647f9b4a285eac6be4356eedf
ssbarbee <ssbarbee@users.noreply.github.com>
2023-08-16 13:57:07 +0200

    fix: add fetchMarginAccountsForWithdrawThunk

--- src/app/features/portfolio/helpers/index.ts
+++ src/app/features/portfolio/helpers/index.ts
@@ -1,3 +1,4 @@
 export * from './getPositionsSummary';
+export * from './mapMarginAccountToMarginAccountUI';
 export * from './mapPortfolioPositionToPortfolioUI';
 export * from './sortPositions';
--- src/app/features/portfolio/thunks/index.ts
+++ src/app/features/portfolio/thunks/index.ts
@@ -1,5 +1,6 @@
 export * from './createMarginAccountThunk';
 export * from './fetchMarginAccountPositionsThunk';
+export * from './fetchMarginAccountsForWithdrawThunk';
 export * from './fetchMarginAccountsThunk';
 export * from './fetchPortfolioSummaryThunk';
 export * from './initialisePortfolioPositionsThunk';
