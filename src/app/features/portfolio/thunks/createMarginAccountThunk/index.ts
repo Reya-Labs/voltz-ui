25c51dc6e9497ef6962967f3bda6ec220b6b8850
ssbarbee <ssbarbee@users.noreply.github.com>
2023-08-17 14:02:37 +0200

    feat: import createMarginAccount from sdk

--- src/app/features/portfolio/thunks/createMarginAccountThunk/index.ts
+++ src/app/features/portfolio/thunks/createMarginAccountThunk/index.ts
@@ -1,31 +1,12 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { providers } from 'ethers';
+import { createMarginAccount, CreateMarginAccountArgs } from '@voltz-protocol/sdk-v2';
+import { ContractReceipt } from 'ethers';
 
 import { rejectThunkWithError } from '../../../helpers';
 
-type CreateMarginAccountArgs = {
-  signer: providers.JsonRpcSigner | null;
-  name: string;
-};
-const createMarginAccount = async ({ name, signer }: CreateMarginAccountArgs) => {
-  return await new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const num = Math.random() * 100;
-      if (num > 50) {
-        reject(new Error('Random error happened. Chance is 50-50 try again!'));
-        return;
-      }
-
-      resolve(true);
-    }, 1000);
-  });
-};
 export const createMarginAccountThunk = createAsyncThunk<
-  Awaited<boolean | ReturnType<typeof rejectThunkWithError>>,
-  {
-    signer: providers.JsonRpcSigner | null;
-    name: string;
-  }
+  Awaited<ContractReceipt | ReturnType<typeof rejectThunkWithError>>,
+  CreateMarginAccountArgs
 >('portfolio/createMarginAccount', async ({ name, signer }, thunkAPI) => {
   if (!signer || !name) {
     return false;
