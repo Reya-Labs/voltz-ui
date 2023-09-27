0484ece743f2cc6873fe18a82ff5ed9d8457a713
ssbarbee <ssbarbee@users.noreply.github.com>
2023-09-27 13:51:56 +0200

    feat: add NotionalSwapHorizontalUI

---
+++ src/ui/components/NotionalSwapHorizontalUI/NotionalSwapHorizontalUI.stories.tsx
@@ -0,0 +1,34 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+
+import { NotionalSwapHorizontalUI } from '.';
+
+export default {
+  title: 'Components/NotionalSwapHorizontalUI',
+  component: NotionalSwapHorizontalUI,
+  args: {},
+} as ComponentMeta<typeof NotionalSwapHorizontalUI>;
+
+const Template: ComponentStory<typeof NotionalSwapHorizontalUI> = (args) => (
+  <NotionalSwapHorizontalUI {...args} />
+);
+
+export const Default = Template.bind({});
+const args: React.ComponentProps<typeof NotionalSwapHorizontalUI> = {
+  fixedRateInfo: 5.49,
+  variableRateInfo: 2.49,
+  loading: false,
+  onModeChange: () => {},
+  mode: 'fixed',
+};
+Default.args = args;
+
+export const WithLoading = Template.bind({});
+const withLoadingArgs: React.ComponentProps<typeof NotionalSwapHorizontalUI> = {
+  fixedRateInfo: 5.49,
+  variableRateInfo: 2.49,
+  loading: true,
+  onModeChange: () => {},
+  mode: 'fixed',
+};
+WithLoading.args = withLoadingArgs;
